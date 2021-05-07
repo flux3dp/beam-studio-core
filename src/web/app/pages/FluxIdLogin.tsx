850fddaaca7aeac41017172a6e2e2d29e937b086
Dean Sung <a71124725@gmail.com>
2021-05-07 09:53:12 +0800

    Fix pages/FluxIdLogin.tsx

--- src/web/app/pages/FluxIdLogin.tsx
+++ src/web/app/pages/FluxIdLogin.tsx
@@ -1,17 +1,15 @@
+import React, { useEffect } from 'react';
+
 import dialog from 'app/actions/dialog-caller';
-
-
-const React = requireNode('react');
-const { useEffect } = requireNode('react');
 
 // Empty page to show login dialog
 const FluxIdLogin = () => {
-    useEffect(() => {
-        dialog.showLoginDialog(() => {
-            location.hash = '#initialize/connect/select-connection-type';
-        });
-    }, []);
-    return <div className="top-bar"/>;
+  useEffect(() => {
+    dialog.showLoginDialog(() => {
+      location.hash = '#initialize/connect/select-connection-type';
+    });
+  }, []);
+  return <div className="top-bar" />;
 }
 
 export default () => FluxIdLogin;
