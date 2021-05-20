35aa8c0dfac747acf31e3571c7be855c2ec317bc
Dean Sung <a71124725@gmail.com>
2021-05-20 09:44:42 +0800

    Update Alert-And-Progress children key, avoid duplicated animation

--- src/web/app/views/dialogs/Alerts-And-Progress.tsx
+++ src/web/app/views/dialogs/Alerts-And-Progress.tsx
@@ -3,57 +3,58 @@ import classNames from 'classnames';
 
 import Alert from 'app/widgets/Alert';
 import Progress from 'app/widgets/Progress';
-import { AlertProgressContext, AlertProgressContextProvider } from 'app/contexts/Alert-Progress-Context';
+import { AlertProgressContext, IAlertProgressContext } from 'app/contexts/Alert-Progress-Context';
 
-let _contextCaller;
+let contextCaller;
 
 export class AlertsAndProgress extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checkboxChecked: false
-    }
-  };
-
-  componentDidMount() {
-    _contextCaller = this.context;
+  componentDidMount(): void {
+    contextCaller = this.context;
   }
 
-  render() {
+  render(): JSX.Element {
     const { alertProgressStack, popFromStack } = this.context;
-    const components = alertProgressStack.map((alertOrProgress, index) => {
+    let alertCount = 0;
+    let progressCount = 0;
+    const components = alertProgressStack.map((alertOrProgress) => {
       if (alertOrProgress.isProgress) {
+        progressCount += 1;
         return (
           <Progress
-            key={index}
+            key={`progress-${progressCount}`}
             progress={alertOrProgress}
             onClose={popFromStack}
           />
         );
-      } else {
-        return (
-          <Alert
-            key={index}
-            {...alertOrProgress}
-            animationClass={classNames('animate__animated', 'animate__bounceIn')}
-            onClose={popFromStack}
-          />
-        );
       }
-    })
+      alertCount += 1;
+      return (
+        <Alert
+          key={`alert-${alertCount}`}
+          caption={alertOrProgress.caption}
+          iconUrl={alertOrProgress.iconUrl}
+          message={alertOrProgress.message}
+          checkboxText={alertOrProgress.checkboxText}
+          checkboxCallbacks={alertOrProgress.checkboxCallbacks}
+          buttons={alertOrProgress.buttons}
+          animationClass={classNames('animate__animated', 'animate__bounceIn')}
+          onClose={popFromStack}
+        />
+      );
+    });
 
     return (
-      <div className='alerts-container'>
+      <div className="alerts-container">
         {components}
       </div>
     );
   }
-};
+}
 
 AlertsAndProgress.contextType = AlertProgressContext;
 
-export class AlertsAndProgressContextHelper {
-  static get context(): AlertProgressContextProvider {
-    return _contextCaller;
-  }
-}
+export const AlertsAndProgressContextHelper = {
+  get context(): IAlertProgressContext {
+    return contextCaller;
+  },
+};
