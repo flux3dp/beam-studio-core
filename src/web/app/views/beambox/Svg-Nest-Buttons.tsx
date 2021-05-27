f7eb5ffe01eb886ff42d38dc8027181b4109576e
Dean Sung <a71124725@gmail.com>
2021-05-27 12:01:59 +0800

    Fix eslint

--- src/web/app/views/beambox/Svg-Nest-Buttons.tsx
+++ src/web/app/views/beambox/Svg-Nest-Buttons.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-continue */
 import i18n from 'helpers/i18n';
 import * as React from 'react';
 import Alert from 'app/actions/alert-caller';
@@ -6,8 +9,9 @@ import Constant from 'app/actions/beambox/constant';
 import Modal from 'app/widgets/Modal';
 import { getSVGAsync } from 'helpers/svg-editor-helper';
 
-let svgCanvas, svgedit;
-getSVGAsync((globalSVG) => { svgCanvas = globalSVG.Canvas; svgedit = globalSVG.Edit });
+let svgCanvas;
+let svgedit;
+getSVGAsync((globalSVG) => { svgCanvas = globalSVG.Canvas; svgedit = globalSVG.Edit; });
 
 const LANG = i18n.lang.beambox.tool_panels;
 
@@ -24,236 +28,253 @@ class SvgNestButtons extends React.Component<Props, State> {
 
   private nestedElements: any[];
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isWorking: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isWorking: false,
+    };
+  }
+
+  nestElements = (elements: Element[], containerElem?: HTMLElement, config?: any) => {
+    let containerPoints;
+    // eslint-disable-next-line prefer-destructuring, @typescript-eslint/dot-notation
+    const ClipperLib = window['ClipperLib'];
+    if (containerElem) {
+      const containerDpath = svgedit.utilities.getPathDFromElement(containerElem);
+      const bbox = svgedit.utilities.getBBox(containerElem);
+      const rotation = {
+        angle: svgedit.utilities.getRotationAngle(containerElem),
+        cx: bbox.x + bbox.width / 2,
+        cy: bbox.y + bbox.height / 2,
+      };
+      containerPoints = ClipperLib.dPathtoPointPathsAndScale(containerDpath, rotation, 1);
+    } else {
+      const w = Constant.dimension.getWidth(BeamboxPreference.read('workarea'));
+      const h = Constant.dimension.getHeight(BeamboxPreference.read('workarea'));
+      containerPoints = [{ x: 0, y: 0 }, { x: w, y: 0 }, { x: w, y: h }, { x: 0, y: h }];
     }
 
-    nestElements = (elements, containerElem?: HTMLElement, config?: any) => {
-        let containerPoints;
-        const ClipperLib = window['ClipperLib'];
-        if (containerElem) {
-            const containerDpath = svgedit.utilities.getPathDFromElement(containerElem);
-            const bbox = svgedit.utilities.getBBox(containerElem);
-            let rotation = {
-                angle: svgedit.utilities.getRotationAngle(containerElem),
-                cx: bbox.x + bbox.width / 2,
-                cy: bbox.y + bbox.height / 2
-            };
-            containerPoints = ClipperLib.dPathtoPointPathsAndScale(containerDpath, rotation, 1);
-        } else {
-            const w = Constant.dimension.getWidth(BeamboxPreference.read('workarea'));
-            const h = Constant.dimension.getHeight(BeamboxPreference.read('workarea'));
-            containerPoints = [{x: 0, y: 0}, {x: w, y: 0}, {x: w, y: h}, {x: 0, y: h}];
+    const elemPoints = [];
+    this.undoNestChanges = [];
+    this.nestedElements = [...elements];
+    for (let i = 0; i < elements.length; i += 1) {
+      let elem = elements[i];
+      if (!elem) continue;
+
+      let bbox;
+      const id = elem.getAttribute('id');
+
+      switch (elem.tagName) {
+        case 'use':
+          bbox = svgCanvas.getSvgRealLocation(elem);
+          break;
+        case 'filter':
+          continue;
+        default:
+          bbox = svgCanvas.calculateTransformedBBox(elem);
+          break;
+      }
+
+      const rotation = {
+        angle: svgedit.utilities.getRotationAngle(elem),
+        cx: bbox.x + bbox.width / 2,
+        cy: bbox.y + bbox.height / 2,
+      };
+
+      let points;
+      const d = svgedit.utilities.getPathDFromElement(elem);
+      if (d) {
+        const pointPaths = ClipperLib.dPathtoPointPathsAndScale(d, rotation, 1);
+        // eslint-disable-next-line prefer-destructuring
+        points = pointPaths[0];
+        for (let j = 1; j < pointPaths.length; j += 1) {
+          if (!points.children) {
+            points.children = [];
+          }
+          points.children.push(pointPaths[j]);
         }
-
-        const elemPoints = [];
-        this.undoNestChanges = [];
-        this.nestedElements = [...elements];
-        for (let i = 0; i < elements.length; i++) {
-            let elem = elements[i];
-            if (!elem) continue;
-
-            let bbox;
-            const id = elem.getAttribute('id');
-
-            switch(elem.tagName) {
-                case 'use':
-                    bbox = svgCanvas.getSvgRealLocation(elem);
-                    break;
-                case 'filter':
-                    continue;
-                default:
-                    bbox = svgCanvas.calculateTransformedBBox(elem);
-                    break;
-            }
-
-            const rotation = {
-                angle: svgedit.utilities.getRotationAngle(elem),
-                cx: bbox.x + bbox.width / 2,
-                cy: bbox.y + bbox.height / 2
-            };
-
-            let points;
-            const d = svgedit.utilities.getPathDFromElement(elem);
-            if (d) {
-                const pointPaths = ClipperLib.dPathtoPointPathsAndScale(d, rotation, 1);
-                points = pointPaths[0];
-                for (let j = 1; j < pointPaths.length; j++) {
-                    if(!points.children) {
-                        points.children = [];
-                    }
-                    points.children.push(pointPaths[j])
-                }
-            } else {
-                points = [{x: bbox.x, y: bbox.y}, {x: bbox.x + bbox.width, y: bbox.y}, {x: bbox.x + bbox.width, y: bbox.y + bbox.height}, {x: bbox.x, y: bbox.y + bbox.height}];
-                if (rotation.angle) {
-                    const rad = rotation.angle * Math.PI / 180;
-                    points = points.map((p) => {
-                        let x = p.x - rotation.cx;
-                        let y = p.y - rotation.cy;
-                        const new_x = x * Math.cos(rad) - y * Math.sin(rad) + rotation.cx;
-                        const new_y = y * Math.cos(rad) + x * Math.sin(rad) + rotation.cy;
-                        return {x: new_x, y: new_y};
-                    });
-                }
-            }
-
-            points.source = id;
-            points.id = elemPoints.length;
-
-            // SvgNest does not support 2 points line segment, here is a hacking to allow the arrangement
-            if (points.length === 2) {
-                points.push(points[1]);
-            }
-
-            elemPoints.push(points);
-
-            let elementsToUndo = [elem];
-            while (elementsToUndo.length > 0) {
-                elem = elementsToUndo.pop();
-                if (elem.nodeType !== 1) continue;
-                let undoRecord: {
-                    element: any,
-                    attrs: any
-                } = {
-                    element: elem,
-                    attrs: {
-                        transform: elem.getAttribute('transform')
-                    }
-                }
-                switch (elem.tagName) {
-                    case 'path':
-                        undoRecord.attrs.d = elem.getAttribute('d');
-                    case 'polygon':
-                        undoRecord.attrs.points = elem.getAttribute('points');
-                    case 'ellipse':
-                        undoRecord.attrs.cx = elem.getAttribute('cx');
-                        undoRecord.attrs.cy = elem.getAttribute('cy');
-                    default:
-                        undoRecord.attrs.x = elem.getAttribute('x');
-                        undoRecord.attrs.y = elem.getAttribute('y');
-                        break;
-                }
-                elementsToUndo.push(...elem.childNodes);
-                this.undoNestChanges.push(undoRecord);
-            }
+      } else {
+        points = [
+          { x: bbox.x, y: bbox.y },
+          { x: bbox.x + bbox.width, y: bbox.y },
+          { x: bbox.x + bbox.width, y: bbox.y + bbox.height },
+          { x: bbox.x, y: bbox.y + bbox.height },
+        ];
+        if (rotation.angle) {
+          const rad = rotation.angle * (Math.PI / 180);
+          points = points.map((p) => {
+            const x = p.x - rotation.cx;
+            const y = p.y - rotation.cy;
+            const newX = x * Math.cos(rad) - y * Math.sin(rad) + rotation.cx;
+            const newY = y * Math.cos(rad) + x * Math.sin(rad) + rotation.cy;
+            return { x: newX, y: newY };
+          });
         }
-        const SvgNest = window['SvgNest'];
+      }
 
-        if (config) {
-            SvgNest.config(config);
+      points.source = id;
+      points.id = elemPoints.length;
+
+      // SvgNest does not support 2 points line segment, here is a hacking to allow the arrangement
+      if (points.length === 2) {
+        points.push(points[1]);
+      }
+
+      elemPoints.push(points);
+
+      const elementsToUndo: any[] = [elem];
+      while (elementsToUndo.length > 0) {
+        elem = elementsToUndo.pop();
+        if (elem.nodeType !== 1) continue;
+        const undoRecord = {
+          element: elem,
+          attrs: {
+            transform: elem.getAttribute('transform'),
+          } as { [key: string]: string },
+        };
+        switch (elem.tagName) {
+          case 'path':
+            undoRecord.attrs.d = elem.getAttribute('d');
+            break;
+          case 'polygon':
+            undoRecord.attrs.points = elem.getAttribute('points');
+            break;
+          case 'ellipse':
+            undoRecord.attrs.cx = elem.getAttribute('cx');
+            undoRecord.attrs.cy = elem.getAttribute('cy');
+            break;
+          default:
+            undoRecord.attrs.x = elem.getAttribute('x');
+            undoRecord.attrs.y = elem.getAttribute('y');
+            break;
         }
-        SvgNest.nestElements(containerPoints, elemPoints);
+        elementsToUndo.push(...elem.childNodes);
+        this.undoNestChanges.push(undoRecord);
+      }
     }
+    // eslint-disable-next-line prefer-destructuring, @typescript-eslint/dot-notation
+    const SvgNest = window['SvgNest'];
 
-    stopNestElement = () => {
-        const SvgNest = window['SvgNest'];
-        SvgNest.stop();
-        let batchCmd = new svgedit.history.BatchCommand('Svg Nest');
-        for(let i = 0; i < this.undoNestChanges.length; i++) {
-            let elem = this.undoNestChanges[i].element;
-            const subCmd = new svgedit.history.ChangeElementCommand(elem, this.undoNestChanges[i].attrs);
-            batchCmd.addSubCommand(subCmd);
-        }
-        if (!batchCmd.isEmpty()) {
-            svgCanvas.undoMgr.addCommandToHistory(batchCmd);
-        }
-        svgCanvas.selectOnly(this.nestedElements);
-        if (this.nestedElements.length > 1) {
-            svgCanvas.tempGroupSelectedElements();
-        }
-        this.nestedElements = null;
+    if (config) {
+      SvgNest.config(config);
     }
+    SvgNest.nestElements(containerPoints, elemPoints);
+  };
 
-    _close = () => {
-        if (this.state.isWorking) {
-            this.stopNestElement();
-        }
-        this.props.onClose();
+  stopNestElement = (): void => {
+    // eslint-disable-next-line prefer-destructuring, @typescript-eslint/dot-notation
+    const SvgNest = window['SvgNest'];
+    SvgNest.stop();
+    const batchCmd = new svgedit.history.BatchCommand('Svg Nest');
+    for (let i = 0; i < this.undoNestChanges.length; i += 1) {
+      const elem = this.undoNestChanges[i].element;
+      const subCmd = new svgedit.history.ChangeElementCommand(elem, this.undoNestChanges[i].attrs);
+      batchCmd.addSubCommand(subCmd);
     }
+    if (!batchCmd.isEmpty()) {
+      svgCanvas.undoMgr.addCommandToHistory(batchCmd);
+    }
+    svgCanvas.selectOnly(this.nestedElements);
+    if (this.nestedElements.length > 1) {
+      svgCanvas.tempGroupSelectedElements();
+    }
+    this.nestedElements = null;
+  };
 
-    onStartOrStop = () => {
-        const {isWorking} = this.state;
-        if (!isWorking) {
-            if (svgCanvas.getTempGroup()) {
-                let children = svgCanvas.ungroupTempGroup();
-                svgCanvas.selectOnly(children, false);
+  close = (): void => {
+    const { onClose } = this.props;
+    const { isWorking } = this.state;
+    if (isWorking) {
+      this.stopNestElement();
+    }
+    onClose();
+  };
+
+  onStartOrStop = (): void => {
+    const { isWorking } = this.state;
+    if (!isWorking) {
+      if (svgCanvas.getTempGroup()) {
+        const children = svgCanvas.ungroupTempGroup();
+        svgCanvas.selectOnly(children, false);
+      }
+      const elems = svgCanvas.getSelectedElems().filter((e) => e);
+      svgCanvas.clearSelection();
+      if (elems.length === 0) {
+        // Empty use all elements
+        const drawing = svgCanvas.getCurrentDrawing();
+        const layerNumber = drawing.getNumLayers();
+        for (let i = 0; i < layerNumber; i += 1) {
+          const name = drawing.getLayerName(i);
+          const layer = drawing.getLayerByName(name);
+          if (layer.getAttribute('display') === 'none' || layer.getAttribute('data-lock') === 'true') {
+            continue;
+          }
+          const { childNodes } = layer;
+          const children = $(layer).children();
+          for (let j = 0; j < childNodes.length; j += 1) {
+            if (!['title', 'filter'].includes(children[j].nodeName)) {
+              elems.push(children[j]);
             }
-            let elems = svgCanvas.getSelectedElems().filter((e) => e);
-            svgCanvas.clearSelection();
-            if (elems.length === 0) {
-                //Empty use all elements
-                const drawing = svgCanvas.getCurrentDrawing();
-                const layerNumber = drawing.getNumLayers();
-                for (let i = 0; i < layerNumber; i++) {
-                    const name = drawing.getLayerName(i);
-                    const layer = drawing.getLayerByName(name);
-                    if (layer.getAttribute('display') === 'none' || layer.getAttribute('data-lock') === 'true') {
-                        continue;
-                    }
-                    const childNodes = layer.childNodes;
-                    const children = $(layer).children();
-                    for (let j = 0; j < childNodes.length; j++) {
-                        if (!['title', 'filter'].includes(children[j].nodeName)) {
-                            elems.push(children[j])
-                        }
-                    }
-                }
-            }
-            console.log(elems);
-            if (elems.length === 0) {
-                Alert.popUp({
-                    caption: LANG.nest,
-                    message: LANG._nest.no_element
-                });
-                return;
-            }
-            this.nestElements(elems);
-        } else {
-            this.stopNestElement();
+          }
         }
-        this.setState({
-            isWorking: !isWorking
+      }
+      // eslint-disable-next-line no-console
+      console.log(elems);
+      if (elems.length === 0) {
+        Alert.popUp({
+          caption: LANG.nest,
+          // eslint-disable-next-line no-underscore-dangle
+          message: LANG._nest.no_element,
         });
+        return;
+      }
+      this.nestElements(elems);
+    } else {
+      this.stopNestElement();
     }
+    this.setState({
+      isWorking: !isWorking,
+    });
+  };
 
-    renderStartButton = () => {
-        const imgSrc = this.state.isWorking ? 'img/beambox/spin.svg' : 'img/beambox/start.svg';
-        const label = this.state.isWorking ? LANG._nest.stop_nest : LANG._nest.start_nest;
-        return (
-            <div className='svg-nest-button active' onClick={() => {this.onStartOrStop()}}>
-                <img src={imgSrc} draggable="false"/>
-                <div className='text with-img'>{label}</div>
-            </div>
-        );
-    }
+  renderStartButton = (): JSX.Element => {
+    const { isWorking } = this.state;
+    const imgSrc = isWorking ? 'img/beambox/spin.svg' : 'img/beambox/start.svg';
+    // eslint-disable-next-line no-underscore-dangle
+    const label = isWorking ? LANG._nest.stop_nest : LANG._nest.start_nest;
+    return (
+      <div className="svg-nest-button active" onClick={this.onStartOrStop}>
+        <img src={imgSrc} draggable="false" />
+        <div className="text with-img">{label}</div>
+      </div>
+    );
+  };
 
-    render() {
-        const { isWorking } = this.state;
-        if (isWorking) {
-            return (
-                <Modal className={{'no-background': true}}>
-                    <div className={`svg-nest-buttons`}>
-                        {this.renderStartButton()}
-                        <div className='svg-nest-button' onClick={() => this._close()}>
-                            <div className={'text'}>{LANG._nest.end}</div>
-                        </div>
-                    </div>
-                </Modal>
-            );
-        }
-        return (
-            <div className={`svg-nest-buttons`}>
-                {this.renderStartButton()}
-                <div className='svg-nest-button' onClick={() => this._close()}>
-                    <div className={'text'}>{LANG._nest.end}</div>
-                </div>
+  render(): JSX.Element {
+    const { isWorking } = this.state;
+    // eslint-disable-next-line no-underscore-dangle
+    const endText = LANG._nest.end;
+    if (isWorking) {
+      return (
+        <Modal className={{ 'no-background': true }}>
+          <div className="svg-nest-buttons">
+            {this.renderStartButton()}
+            <div className="svg-nest-button" onClick={this.close}>
+              <div className="text">{endText}</div>
             </div>
-        );
+          </div>
+        </Modal>
+      );
     }
-};
+    return (
+      <div className="svg-nest-buttons">
+        {this.renderStartButton()}
+        <div className="svg-nest-button" onClick={this.close}>
+          <div className="text">{endText}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SvgNestButtons;
