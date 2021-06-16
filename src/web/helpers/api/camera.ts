5fc72796e8ab64bef74dd1e47de10ac6112e8ac2
Dean Sung <a71124725@gmail.com>
2021-06-16 15:10:04 +0800

    Update camera transmit fail judging

--- src/web/helpers/api/camera.ts
+++ src/web/helpers/api/camera.ts
@@ -1,159 +1,182 @@
+/* eslint-disable no-console */
 /**
  * API camera
  * Ref: https://github.com/flux3dp/fluxghost/wiki/websocket-camera(monitoring)
  */
 import { Subject, from, ObservableInput } from 'rxjs';
-import { concatMap, filter, map, take, timeout } from 'rxjs/operators';
+import {
+  concatMap, filter, map, take, timeout,
+} from 'rxjs/operators';
 
-import Websocket from '../websocket';
-import rsaKey from '../rsa-key';
-import VersionChecker from '../version-checker';
 import i18n from 'helpers/i18n';
+import rsaKey from 'helpers/rsa-key';
+import VersionChecker from 'helpers/version-checker';
+import Websocket from 'helpers/websocket';
+import { IDeviceInfo } from 'interfaces/IDevice';
 
 const TIMEOUT = 30000;
-const MIN_IMAGE_ALLOWABLE_SIZE = 10000;
-const IMAGE_TRANSMISSION_FAIL_THRESHOLD = 40;
-const CAMERA_CABLE_ALERT_THRESHOLD = 20;
+const IMAGE_TRANSMISSION_FAIL_THRESHOLD = 20;
+const CAMERA_CABLE_ALERT_THRESHOLD = 10;
 const LANG = i18n.lang;
+
+const checkBlobIsImage = (blob: Blob, failTime = 3000) => new Promise<boolean>((resolve) => {
+  const img = new Image();
+  const imgUrl = URL.createObjectURL(blob);
+  let resolved = false;
+  const resolveAndCleanUp = (result: boolean) => {
+    resolved = true;
+    URL.revokeObjectURL(imgUrl);
+    resolve(result);
+  };
+  const failTimer = setTimeout(() => {
+    if (!resolved) {
+      resolveAndCleanUp(false);
+    }
+  }, failTime);
+  img.onload = () => {
+    if (!resolved) {
+      resolveAndCleanUp(true);
+      clearTimeout(failTimer);
+    }
+  };
+  img.src = imgUrl;
+});
 
 class Camera {
   cameraNeedFlip: boolean = null;
+
   shouldCrop: boolean;
-  private _device: {
+
+  private device: {
     uuid: string | null,
     source: string | null,
     model: string | null,
     version: string | null
   };
-  private _ws: any;
-  private _wsSubject: Subject<any>;
-  private _source: any;
+
+  private ws: any;
+
+  private wsSubject: Subject<any>;
+
+  private source: any;
+
   private requireFrameRetry: number;
-  private needCameraCableAlert: boolean;
-  constructor(shouldCrop: boolean = true, cameraNeedFlip: boolean = null) {
+
+  constructor(shouldCrop = true, cameraNeedFlip: boolean = null) {
     this.shouldCrop = shouldCrop;
-    this._device = {
+    this.device = {
       uuid: null,
       source: null,
       model: null,
-      version: null
+      version: null,
     };
     if (cameraNeedFlip !== null) {
       this.cameraNeedFlip = cameraNeedFlip;
     }
-    this._ws = null;
+    this.ws = null;
     this.requireFrameRetry = 0;
-    this.needCameraCableAlert = false;
-    this._wsSubject = new Subject<Blob>();
-    this._source = this._wsSubject
+    this.wsSubject = new Subject<Blob>();
+    this.source = this.wsSubject
       .asObservable()
-      .pipe(filter(x => x instanceof Blob))
-      .pipe(filter((blob: Blob) => {
-        // if stream return extremely small blob (i.e. when camera hardware connection fail)
-        if (blob.size >= MIN_IMAGE_ALLOWABLE_SIZE) {
-          this.needCameraCableAlert = this.requireFrameRetry >= CAMERA_CABLE_ALERT_THRESHOLD;
-          this.requireFrameRetry = 0;
-          return true;
-        }
-        // TODO: Add camera cable alert
-        console.log('Blob size:', blob.size, 'too small.\nRetry time:', this.requireFrameRetry);
-        if (this.requireFrameRetry < IMAGE_TRANSMISSION_FAIL_THRESHOLD) {
-          setTimeout(() => this._ws.send('require_frame'), 500);
-          this.requireFrameRetry += 1;
-          return false;
-        }
-        throw new Error(LANG.message.camera.fail_to_transmit_image);
-      }))
+      .pipe(filter((x) => x instanceof Blob))
       .pipe(map(async (blob: Blob) => {
+        const isImage = await checkBlobIsImage(blob);
+        if (!isImage) {
+          if (this.requireFrameRetry < IMAGE_TRANSMISSION_FAIL_THRESHOLD) {
+            setTimeout(() => this.ws.send('require_frame'), 500);
+            this.requireFrameRetry += 1;
+            return null;
+          }
+          throw new Error(LANG.message.camera.fail_to_transmit_image);
+        }
+        const needCameraCableAlert = this.requireFrameRetry >= CAMERA_CABLE_ALERT_THRESHOLD;
+        this.requireFrameRetry = 0;
         const imgBlob = await this.preprocessImage(blob);
-        const { needCameraCableAlert } = this;
         return { imgBlob, needCameraCableAlert };
       }))
-      .pipe(concatMap((p: ObservableInput<any>) => from(p)));
+      .pipe(concatMap((p: ObservableInput<any>) => from(p)
+        .pipe(filter((result) => result))));
   }
 
   // let subject get response from websocket
-  async createWs(device) {
-    this._device = device;
-    console.log("Device ", device);
+  async createWs(device: IDeviceInfo): Promise<void> {
+    this.device = device;
+    console.log('Device ', device);
     console.assert(device.version, 'device miss version!', device);
-    const method = (device.source === 'h2h') ? `camera/usb/${parseInt(device.uuid)}` : `camera/${device.uuid}`;
+    const method = (device.source === 'h2h') ? `camera/usb/${parseInt(device.uuid, 10)}` : `camera/${device.uuid}`;
 
-    this._ws = Websocket({
-      method: method,
-      onOpen: () => this._ws.send(rsaKey()),
-      onMessage: (res) => this._wsSubject.next(res),
-      onError: (res) => {
-        return this._wsSubject.error(new Error(`Camera WS ${res.error ? res.error.toString() : res}`))
-      },
-      onFatal: (res) => {
-        return this._wsSubject.error(new Error(`Camera WS ${res.error ? res.error.toString() : res}`))
-      },
-      onClose: () => this._wsSubject.complete(),
+    this.ws = Websocket({
+      method,
+      onOpen: () => this.ws.send(rsaKey()),
+      onMessage: (res) => this.wsSubject.next(res),
+      onError: (res) => this.wsSubject.error(new Error(`Camera WS ${res.error ? res.error.toString() : res}`)),
+      onFatal: (res) => this.wsSubject.error(new Error(`Camera WS ${res.error ? res.error.toString() : res}`)),
+      onClose: () => this.wsSubject.complete(),
       autoReconnect: false,
     });
 
-    // if response.status === 'connected' within TIMEOUT, the promise resolve. and the websocket will keep listening.
-    await this._wsSubject
-      .pipe(filter(res => res.status === 'connected'))
+    // if response.status === 'connected' within TIMEOUT,
+    // the promise resolve. and the websocket will keep listening.
+    await this.wsSubject
+      .pipe(filter((res) => res.status === 'connected'))
       .pipe(take(1))
       .pipe(timeout(TIMEOUT))
       .toPromise();
 
     // check whether the camera need flip
-    if (this.cameraNeedFlip === null && device && device['model'].indexOf('delta-') < 0) {
-      this.cameraNeedFlip = !!(Number((/F:\s?(\-?\d+\.?\d+)/.exec(await this._getCameraOffset()) || ['', ''])[1]));
+    if (this.cameraNeedFlip === null && device && device.model.indexOf('delta-') < 0) {
+      this.cameraNeedFlip = !!(Number((/F:\s?(-?\d+\.?\d+)/.exec(await this.getCameraOffset()) || ['', ''])[1]));
     }
   }
 
-  async _getCameraOffset() {
+  async getCameraOffset(): Promise<string> {
     console.warn('This is additional control socket created in camera.ts, this may take unnecessary time.');
-    const tempWsSubject = new Subject<{error?: Error, status: string, value: string}>();
+    const tempWsSubject = new Subject<{ error?: Error, status: string, value: string }>();
     const tempWs = Websocket({
-      method: (this._device.source === 'h2h') ? `control/usb/${parseInt(this._device.uuid)}` : `control/${this._device.uuid}`,
+      method: (this.device.source === 'h2h') ? `control/usb/${parseInt(this.device.uuid, 10)}` : `control/${this.device.uuid}`,
       onOpen: () => tempWs.send(rsaKey()),
       onMessage: (res) => tempWsSubject.next(res),
       onError: (res) => tempWsSubject.error(new Error(res.error ? res.error.toString() : res)),
       onFatal: (res) => tempWsSubject.error(new Error(res.error ? res.error.toString() : res)),
       onClose: () => tempWsSubject.complete(),
-      autoReconnect: false
+      autoReconnect: false,
     });
     await tempWsSubject
-      .pipe(filter(res => res.status === 'connected'))
+      .pipe(filter((res) => res.status === 'connected'))
       .pipe(take(1))
       .pipe(timeout(TIMEOUT))
       .toPromise();
 
     tempWs.send('config get camera_offset');
-    const camera_offset = await tempWsSubject
+    const cameraOffset = await tempWsSubject
       .pipe(take(1))
       .pipe(timeout(TIMEOUT))
       .toPromise();
-    return camera_offset.value;
+    return cameraOffset.value;
   }
 
   async oneShot() {
-    if (this._wsSubject.isStopped) {
-      if (this._wsSubject.hasError) {
-        console.error(this._wsSubject.thrownError);
+    if (this.wsSubject.isStopped) {
+      if (this.wsSubject.hasError) {
+        console.error(this.wsSubject.thrownError);
       }
       throw new Error(LANG.message.camera.ws_closed_unexpectly);
     }
-    this._ws.send('require_frame');
-    return await this._source
+    this.ws.send('require_frame');
+    return await this.source
       .pipe(take(1))
       .pipe(timeout(TIMEOUT))
       .toPromise();
   }
 
   getLiveStreamSource() {
-    this._ws.send('enable_streaming');
-    return this._source
+    this.ws.send('enable_streaming');
+    return this.source
       .pipe(timeout(TIMEOUT));
   }
 
-  closeWs() {
-    this._ws.close(false);
+  closeWs(): void {
+    this.ws.close(false);
   }
 
   async preprocessImage(blob: Blob) {
@@ -161,8 +184,10 @@ class Camera {
     const imageLoadBlob = async () => {
       const img = new Image();
       const imgUrl = URL.createObjectURL(blob);
-      img.src = imgUrl;
-      await new Promise(resolve => img.onload = resolve);
+      await new Promise((resolve) => {
+        img.onload = resolve;
+        img.src = imgUrl;
+      });
       URL.revokeObjectURL(imgUrl);
 
       const canvas = document.createElement('canvas');
@@ -185,7 +210,9 @@ class Camera {
       canvas.width = 640;
       canvas.height = 280;
       canvas.getContext('2d').drawImage(img, 0, -40, 640, 360); // resize
-      const preprocessedBlob = await new Promise<Blob>(resolve => canvas.toBlob(b => resolve(b)));
+      const preprocessedBlob = await new Promise<Blob>(
+        (resolve) => canvas.toBlob((b) => resolve(b)),
+      );
       return preprocessedBlob;
     };
 
@@ -197,27 +224,30 @@ class Camera {
       canvas.width = 640;
       canvas.height = 280;
       canvas.getContext('2d').drawImage(img, 0, -100, 640, 480); // crop top and bottom
-      const preprocessedBlob = await new Promise<Blob>(resolve => canvas.toBlob(b => resolve(b)));
+      const preprocessedBlob = await new Promise<Blob>(
+        (resolve) => canvas.toBlob((b) => resolve(b)),
+      );
       return preprocessedBlob;
     };
 
     const loadAndFlipImage = async () => {
       const canvas = await imageLoadBlob();
-      const preprocessedBlob = await new Promise<Blob>(resolve => canvas.toBlob(b => resolve(b)));
+      const preprocessedBlob = await new Promise<Blob>(
+        (resolve) => canvas.toBlob((b) => resolve(b)),
+      );
       return preprocessedBlob;
-    }
+    };
 
-    if (!['mozu1', 'fbm1', 'fbb1b', 'fbb1p', 'fbb2b', 'laser-b1', 'darwin-dev'].includes(this._device.model)) {
+    if (!['mozu1', 'fbm1', 'fbb1b', 'fbb1p', 'fbb2b', 'laser-b1', 'darwin-dev'].includes(this.device.model)) {
       return blob;
     }
     if (!this.shouldCrop) {
       return await loadAndFlipImage();
     }
-    if (VersionChecker(this._device.version).meetRequirement('BEAMBOX_CAMERA_SPEED_UP')) {
+    if (VersionChecker(this.device.version).meetRequirement('BEAMBOX_CAMERA_SPEED_UP')) {
       return await crop640x480ImageTo640x280();
-    } else {
-      return await resize1280x720ImageTo640x280();
     }
+    return await resize1280x720ImageTo640x280();
   }
 }
 
