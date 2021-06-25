cacedff72ee302abbb4d0d12a7c0c44ea32bb9dc
Dean Sung <a71124725@gmail.com>
2021-06-25 14:23:56 +0800

    Format beam-file-helper

--- src/web/helpers/beam-file-helper.ts
+++ src/web/helpers/beam-file-helper.ts
@@ -53,188 +53,249 @@
    ---------------------------------------------------------------------------------
 
 */
+import { Buffer } from 'buffer';
 
 import fs from 'implementations/fileSystem';
 import Progress from 'app/actions/progress-caller';
+import { getSVGAsync } from 'helpers/svg-editor-helper';
 
 const { $ } = window;
+let svgEditor;
+
+getSVGAsync((globalSVG) => {
+  svgEditor = globalSVG.Editor;
+});
 
 // Create VInt Buffer, first bit indicate continue or not, other 7 bits represent value
 const valueToVIntBuffer = (value) => {
-    const a = [];
-    while (value > 127) {
-        const b = value % 128 + 128;
-        a.push(b);
-        value = Math.floor(value / 128);
-    }
-    a.push(value);
-    return Buffer.from(a);
-}
+  const a: number[] = [];
+  let remainingValue = value;
+  while (remainingValue > 127) {
+    const b = (remainingValue % 128) + 128;
+    a.push(b);
+    remainingValue = Math.floor(remainingValue / 128);
+  }
+  a.push(remainingValue);
+  return Buffer.from(a);
+};
 
-const readVInt = (buffer, offset=0) => {
-    let v = 0;
-    let currentByte = 0;
-    while (true) {
-        const b = buffer.readUInt8(offset);
-        offset += 1;
-        v += (b % 128) * 2 ** (7 * currentByte);
-        currentByte += 1;
-        if (b < 128) break;
-    }
-    return {
-        value: v,
-        offset
-    };
-}
+const readVInt = (buffer, offset = 0) => {
+  let v = 0;
+  let currentByte = 0;
+  let currentOffset = offset;
+  while (true) {
+    const b = buffer.readUInt8(currentOffset);
+    currentOffset += 1;
+    v += (b % 128) * 2 ** (7 * currentByte);
+    currentByte += 1;
+    if (b < 128) break;
+  }
+  return {
+    value: v,
+    offset: currentOffset,
+  };
+};
 
-const localHeaderTypeBuffer = (type) => {
-    switch (type) {
-        case 'svgContent':
-            return Buffer.from([0x01]);
-        case 'imageSource':
-            return Buffer.from([0x02]);
-    }
-}
+const localHeaderTypeBuffer = (type): Buffer => {
+  switch (type) {
+    case 'svgContent':
+      return Buffer.from([0x01]);
+    case 'imageSource':
+      return Buffer.from([0x02]);
+    default:
+      break;
+  }
+  return Buffer.from([]);
+};
 
 // 1 Byte Type (0x01 for svg content) + ? bytes vint length + length bytes svg string
 const genertateSvgBlockBuffer = (svgString) => {
-    const typeBuf = localHeaderTypeBuffer('svgContent');
-    const svgStringBuf = Buffer.from(svgString);
-    const lengthVintBuf = valueToVIntBuffer(svgStringBuf.length);
-    return Buffer.concat([typeBuf, lengthVintBuf, svgStringBuf]);
-}
+  const typeBuf = localHeaderTypeBuffer('svgContent');
+  const svgStringBuf = Buffer.from(svgString);
+  const lengthVintBuf = valueToVIntBuffer(svgStringBuf.length);
+  return Buffer.concat([typeBuf, lengthVintBuf, svgStringBuf]);
+};
 
 // 1 Byte Type (0x02 for svg content) + ? bytes vint length + length bytes svg string
-const generateImageSourceBlockBuffer = (imageSources) => {
-    let imageSourceBlockBuffer = localHeaderTypeBuffer('imageSource');
-    let tempbuffer = Buffer.alloc(0);
-    for (let id in imageSources) {
-        const idSizeBuf = Buffer.alloc(1);
-        const idBuf = Buffer.from(id);
-        idSizeBuf.writeUInt8(idBuf.length, 0);
-        const imageBuf = Buffer.from(imageSources[id]);
-        const imageSizeBuf = valueToVIntBuffer(imageBuf.length);
-        tempbuffer = Buffer.concat([tempbuffer, idSizeBuf, idBuf, imageSizeBuf, imageBuf]);
-    }
-    imageSourceBlockBuffer = Buffer.concat([imageSourceBlockBuffer, valueToVIntBuffer(tempbuffer.length), tempbuffer]);
-    return imageSourceBlockBuffer;
-}
+const generateImageSourceBlockBuffer = (imageSources: { [id: string]: ArrayBuffer }) => {
+  let imageSourceBlockBuffer = localHeaderTypeBuffer('imageSource');
+  let tempbuffer = Buffer.alloc(0);
+  const ids = Object.keys(imageSources);
+  for (let i = 0; i < ids.length; i += 1) {
+    const id = ids[i];
+    const idSizeBuf = Buffer.alloc(1);
+    const idBuf = Buffer.from(id);
+    idSizeBuf.writeUInt8(idBuf.length, 0);
+    const imageBuf = Buffer.from(imageSources[id]);
+    const imageSizeBuf = valueToVIntBuffer(imageBuf.length);
+    tempbuffer = Buffer.concat([tempbuffer, idSizeBuf, idBuf, imageSizeBuf, imageBuf]);
+  }
+  imageSourceBlockBuffer = Buffer.concat([
+    imageSourceBlockBuffer,
+    valueToVIntBuffer(tempbuffer.length),
+    tempbuffer,
+  ]);
+  return imageSourceBlockBuffer;
+};
 
-const saveBeam = async (path, svgString, imageSources) => {
-    const signatureBuffer = Buffer.from([66, 101, 97, 109, 2]); // Bvg{version in uint} max to 255
-    const svgBlockBuf = genertateSvgBlockBuffer(svgString);
-    const imageSourceBlockBuffer = generateImageSourceBlockBuffer(imageSources);
-    const metaDataBuf = Buffer.from('Hi, I am meta data O_<');
-    const headerBuffer = Buffer.concat([valueToVIntBuffer(metaDataBuf.length), metaDataBuf, valueToVIntBuffer(svgBlockBuf.length), valueToVIntBuffer(imageSourceBlockBuffer.length)]);
-    const headerSizeBuf = valueToVIntBuffer(headerBuffer.length);
-    fs.writeStream(path, 'w', [
-      Buffer.concat([
-        signatureBuffer,
-        headerSizeBuf,
-        headerBuffer,
-        svgBlockBuf,
-        imageSourceBlockBuffer,
-      ]),
-      Buffer.from([0x00]),
-    ]);
+const getBeamBlob = (svgString: string, imageSources: { [id: string]: ArrayBuffer }): Blob => {
+  const signatureBuffer = Buffer.from([66, 101, 97, 109, 2]); // Bvg{version in uint} max to 255
+  const svgBlockBuf = genertateSvgBlockBuffer(svgString);
+  const imageSourceBlockBuffer = generateImageSourceBlockBuffer(imageSources);
+  const metaDataBuf = Buffer.from('Hi, I am meta data O_<');
+  const headerBuffer = Buffer.concat([
+    valueToVIntBuffer(metaDataBuf.length),
+    metaDataBuf,
+    valueToVIntBuffer(svgBlockBuf.length),
+    valueToVIntBuffer(imageSourceBlockBuffer.length),
+  ]);
+  const headerSizeBuf = valueToVIntBuffer(headerBuffer.length);
+  const buffer = Buffer.concat([
+    signatureBuffer,
+    headerSizeBuf,
+    headerBuffer,
+    svgBlockBuf,
+    imageSourceBlockBuffer,
+    Buffer.from([0x00]),
+  ]);
+  const arrayBuffer = Uint8Array.from(buffer).buffer;
+  const blob = new Blob([arrayBuffer]);
+  return blob;
+};
+
+const saveBeam = async (
+  path: string,
+  svgString: string,
+  imageSources: { [id: string]: ArrayBuffer },
+): Promise<void> => {
+  const signatureBuffer = Buffer.from([66, 101, 97, 109, 2]); // Bvg{version in uint} max to 255
+  const svgBlockBuf = genertateSvgBlockBuffer(svgString);
+  const imageSourceBlockBuffer = generateImageSourceBlockBuffer(imageSources);
+  const metaDataBuf = Buffer.from('Hi, I am meta data O_<');
+  const headerBuffer = Buffer.concat([
+    valueToVIntBuffer(metaDataBuf.length),
+    metaDataBuf,
+    valueToVIntBuffer(svgBlockBuf.length),
+    valueToVIntBuffer(imageSourceBlockBuffer.length),
+  ]);
+  const headerSizeBuf = valueToVIntBuffer(headerBuffer.length);
+  fs.writeStream(path, 'w', [
+    Buffer.concat([
+      signatureBuffer,
+      headerSizeBuf,
+      headerBuffer,
+      svgBlockBuf,
+      imageSourceBlockBuffer,
+    ]),
+    Buffer.from([0x00]),
+  ]);
 };
 
 const readHeader = (headerBuf) => {
-    let vInt;
-    let offset = 0;
-    vInt = readVInt(headerBuf, offset);
-    offset = vInt.offset;
-    let metadataSize = vInt.value;
-    const metaData = headerBuf.toString('utf-8', offset, offset + metadataSize);
-    console.log(metaData);
-    offset += metadataSize;
-    vInt = readVInt(headerBuf, offset);
-    const svgBlockSize = vInt.value;
-    console.log('svgBlockSize', svgBlockSize);
-    offset = vInt.offset;
-    vInt = readVInt(headerBuf, offset);
-    const imageSourceBlockSize = vInt.value;
-    console.log('Image Source block Size', imageSourceBlockSize);
-}
-
-const readBlocks = async (buf, offset) => {
-    if (offset >= buf.length) {
-        console.warn('offset exceed buffer length');
-        return -1;
-    }
-    const blockType = buf.readUInt8(offset);
-    let vint, size;
-    offset += 1;
-    switch (blockType) {
-        //Ending Block
-        case 0:
-            console.log('Ending Block');
-            offset = -1;
-            break;
-        //Svg Content
-        case 1:
-            vint = readVInt(buf, offset);
-            offset = vint.offset;
-            size = vint.value;
-            const svgString = buf.toString('utf-8', offset, offset + size);
-            await window['svgEditor'].importBvgStringAsync(svgString);
-            offset += size;
-            break;
-        case 2:
-            vint = readVInt(buf, offset);
-            offset = vint.offset;
-            size = vint.value;
-            readImageSource(buf, offset, offset + size);
-            offset += size
-            break;
-        default:
-            console.error(`Unknown Block Type: ${blockType}`);
-            offset = -1;
-    }
-    return offset;
-}
+  let vInt;
+  let offset = 0;
+  vInt = readVInt(headerBuf, offset);
+  offset = vInt.offset;
+  const metadataSize = vInt.value;
+  // const metaData = headerBuf.toString('utf-8', offset, offset + metadataSize);
+  // console.log(metaData);
+  offset += metadataSize;
+  vInt = readVInt(headerBuf, offset);
+  // const svgBlockSize = vInt.value;
+  // console.log('svgBlockSize', svgBlockSize);
+  offset = vInt.offset;
+  vInt = readVInt(headerBuf, offset);
+  // const imageSourceBlockSize = vInt.value;
+  // console.log('Image Source block Size', imageSourceBlockSize);
+};
 
 const readImageSource = (buf, offset, end) => {
-    while (offset < end) {
-        const idSize = buf.readUInt8(offset);
-        offset += 1;
-        const id = buf.toString('utf-8', offset, offset + idSize);
-        offset += idSize;
-        const {value: imageSize, offset: newOffset} = readVInt(buf, offset);
-        offset = newOffset;
-        var blob = new Blob([buf.slice(offset, offset + imageSize)]);
-        let src = URL.createObjectURL(blob);
-        offset +=  imageSize;
-        $(`#${id}`).attr('origImage', src);
-    }
-}
+  let currentOffset = offset;
+  while (currentOffset < end) {
+    const idSize = buf.readUInt8(currentOffset);
+    currentOffset += 1;
+    const id = buf.toString('utf-8', currentOffset, currentOffset + idSize);
+    currentOffset += idSize;
+    const { value: imageSize, offset: newOffset } = readVInt(buf, currentOffset);
+    currentOffset = newOffset;
+    const blob = new Blob([buf.slice(currentOffset, currentOffset + imageSize)]);
+    const src = URL.createObjectURL(blob);
+    currentOffset += imageSize;
+    $(`#${id}`).attr('origImage', src);
+  }
+};
 
-const readBeam = async (file) => {
-    let fetchedData = await fetch((typeof file === 'string' ? file : file.path).replace('#', '%23'));
-    const respData = await fetchedData.blob();
-    let data = await new Response(respData).arrayBuffer();
-    const buf = Buffer.from(data);
+const readBlocks = async (buf, offset) => {
+  if (offset >= buf.length) {
+    // eslint-disable-next-line no-console
+    console.warn('offset exceed buffer length');
+    return -1;
+  }
+  let currentOffset = offset;
+  const blockType = buf.readUInt8(currentOffset);
+  let vint;
+  let size;
+  currentOffset += 1;
+  if (blockType === 0) {
+    // Ending Block
+    currentOffset = -1;
+  } else if (blockType === 1) {
+    // Svg Content
+    vint = readVInt(buf, currentOffset);
+    currentOffset = vint.offset;
+    size = vint.value;
+    const svgString = buf.toString('utf-8', currentOffset, currentOffset + size);
+    await svgEditor.importBvgStringAsync(svgString);
+    currentOffset += size;
+  } else if (blockType === 2) {
+    // image source
+    vint = readVInt(buf, currentOffset);
+    currentOffset = vint.offset;
+    size = vint.value;
+    readImageSource(buf, currentOffset, currentOffset + size);
+    currentOffset += size;
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(`Unknown Block Type: ${blockType}`);
+    currentOffset = -1;
+  }
+  return currentOffset;
+};
 
-    let offset = 0, vint;
-    const signatureBuffer = buf.slice(offset, 5);
-    console.log('Signature:', signatureBuffer.toString());
-    offset += 5;
-    const version = signatureBuffer.readUInt8(4);
-    console.log('Beam Version: ', version);
-    vint = readVInt(buf, offset);
-    const headerSize = vint.value;
-    offset = vint.offset;
-    const headerBuf = buf.slice(offset, offset + headerSize);
-    readHeader(headerBuf);
-    offset += headerSize;
-    while(offset > 0) {
-        offset = await readBlocks(buf, offset);
-    }
+const readBeam = async (file: File): Promise<void> => {
+  const data = await new Promise<ArrayBuffer>((resolve) => {
+    const fr = new FileReader();
+    fr.onloadend = (evt) => {
+      resolve(evt.target.result as ArrayBuffer);
+    };
+    fr.readAsArrayBuffer(file);
+  });
+  const buf = Buffer.from(data);
 
-    Progress.popById('loading_image');
+  let offset = 0;
+  const signatureBuffer = buf.slice(offset, 5);
+  // eslint-disable-next-line no-console
+  console.log('Signature:', signatureBuffer.toString());
+  offset += 5;
+  const version = signatureBuffer.readUInt8(4);
+  // eslint-disable-next-line no-console
+  console.log('Beam Version: ', version);
+  const vint = readVInt(buf, offset);
+  const headerSize = vint.value;
+  offset = vint.offset;
+  const headerBuf = buf.slice(offset, offset + headerSize);
+  readHeader(headerBuf);
+  offset += headerSize;
+  while (offset > 0) {
+    // eslint-disable-next-line no-await-in-loop
+    offset = await readBlocks(buf, offset);
+  }
+
+  Progress.popById('loading_image');
 };
 
 export default {
-    saveBeam,
-    readBeam
-}
+  getBeamBlob,
+  saveBeam,
+  readBeam,
+};
