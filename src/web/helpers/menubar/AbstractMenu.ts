5ae76b49dc6b1dd410bac7da54d1f1ec61345125
Yanting Chen <mx.alexender@gmail.com>
2021-06-28 18:13:41 +0800

    1. simplify global interaction 2. move some actions to menu implementation

--- src/web/helpers/menubar/AbstractMenu.ts
+++ src/web/helpers/menubar/AbstractMenu.ts
@@ -5,8 +5,17 @@ import menuDeviceActions from 'app/actions/beambox/menuDeviceActions';
 import menuEventListenerFactory from 'implementations/menuEventListenerFactory';
 import { IDeviceInfo } from 'interfaces/IDevice';
 
+const MENU_ITEMS = ['IMPORT', 'EXPORT_FLUX_TASK', 'SAVE_SCENE',
+  'UNDO', 'DUPLICATE', 'PHOTO_EDIT', 'DOCUMENT_SETTING', 'CLEAR_SCENE',
+  'ZOOM_IN', 'ZOOM_OUT', 'FITS_TO_WINDOW', 'ZOOM_WITH_WINDOW', 'SHOW_GRIDS', 'SHOW_LAYER_COLOR',
+  'NETWORK_TESTING', 'ABOUT_BEAM_STUDIO'];
+
 export default abstract class AbstractMenu {
   abstract init(): void;
+
+  abstract enable(items: string[]): void;
+
+  abstract disable(items: string[]): void;
 
   private menuEventRegistered = false;
 
@@ -44,5 +53,21 @@ export default abstract class AbstractMenu {
     if (!this.menuEventRegistered) {
       registerMenuClickEvents();
     }
+  }
+
+  attach(enabledItems: string[]): void {
+    const disabledItems = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of MENU_ITEMS) {
+      if (enabledItems.indexOf(item) < 0) {
+        disabledItems.push(item);
+      }
+    }
+    this.enable(enabledItems);
+    this.disable(disabledItems);
+  }
+
+  detach(): void {
+    this.disable(MENU_ITEMS);
   }
 }
