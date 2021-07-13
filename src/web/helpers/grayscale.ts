babf42978381126b333a3d3010e81f0a07c9206a
Yanting Chen <ChenYanting@users.noreply.github.com>
2021-07-13 10:21:25 +0800

    add tests (#171)
    * 1. add tests
    2. increase coverage threshold
    3. make coverage check depend on unit tests (it does not make sense to calculate coverage if there is any failed test)
    
    * avoid undefined window.os

--- src/web/helpers/grayscale.ts
+++ src/web/helpers/grayscale.ts
@@ -1,66 +1,41 @@
 /**
  * grayscale
  */
-export default function(data, opts) {
-    opts = opts || {};
-    opts.is_rgba = ('boolean' === typeof opts.is_rgba ? opts.is_rgba : false);
-    opts.threshold = ('number' === typeof opts.threshold ? opts.threshold : 128);
-    opts.is_shading = ('boolean' === typeof opts.is_shading ? opts.is_shading : true);
-    opts.is_svg = ('boolean' === typeof opts.is_svg ? opts.is_svg : false);
-    opts.is_binary = ('boolean' === typeof opts.is_binary ? opts.is_binary : false);
-    var binary = new Uint8Array(opts.is_rgba ? data.length : data.length / 4),
-        WHITE = 255,
-        BLACK = 0,
-        grayscale,
-        i, j,
-        red, green, blue, alpha;
+export default function (data, opts) {
+  opts = opts || {};
+  opts.threshold = (typeof opts.threshold === 'number' ? opts.threshold : 128);
+  opts.is_shading = (typeof opts.is_shading === 'boolean' ? opts.is_shading : true);
+  opts.is_binary = (typeof opts.is_binary === 'boolean' ? opts.is_binary : false);
+  const binary = new Uint8Array(data.length);
+  const WHITE = 255;
+  const BLACK = 0;
 
-    for (i = 0; i < data.length; i += 4) {
-        var binaryIndex = Math.floor(i / 4);
-        if (false === opts.is_svg) {
-            // http://yolijn.com/convert-rgba-to-rgb
-            alpha = data[i + 3] / 255;
-            // refers to http://en.wikipedia.org/wiki/Grayscale
-            grayscale = (1 - alpha) * WHITE + alpha * Math.round(0.299 * data[i] + 0.587 * data[i+1] + 0.114 * data[i+2]);
+  for (let i = 0; i < data.length; i += 4) {
+    const getGrayScale = (alpha, r, g, b) => (1 - alpha) * WHITE + alpha * Math.round(0.299 * r + 0.587 * g + 0.114 * b);
+    // http://yolijn.com/convert-rgba-to-rgb
+    const alpha = data[i + 3] / 255;
+    // refers to http://en.wikipedia.org/wiki/Grayscale
+    let grayscale = getGrayScale(alpha, data[i], data[i + 1], data[i + 2]);
 
-            // is shading?
-            if (false === opts.is_shading && opts.threshold > grayscale) {
-                grayscale = BLACK;
-            }
+    if (opts.is_binary) {
+      binary[i] = grayscale > opts.threshold ? 255 : 0;
+      binary[i + 1] = grayscale > opts.threshold ? 255 : 0;
+      binary[i + 2] = grayscale > opts.threshold ? 255 : 0;
+      binary[i + 3] = 255;
+    } else {
+      // is shading?
+      if (!opts.is_shading && opts.threshold > grayscale) {
+        grayscale = BLACK;
+      }
 
-            grayscale = (opts.threshold > grayscale ? grayscale : WHITE);
+      grayscale = (opts.threshold > grayscale ? grayscale : WHITE);
 
-            if (false === opts.is_rgba) {
-                binary[binaryIndex] = data[i + 3] === 0 ? WHITE : grayscale;
-            }
-            else if (opts.is_binary) {
-                const grayscaleValue = (1 - alpha) * WHITE + alpha * Math.round(0.299 * data[i] + 0.587 * data[i+1] + 0.114 * data[i+2]);
-
-                if (grayscaleValue > opts.threshold) {
-                    binary[i] = binary[i+1] = binary[i+2] = binary[i+3] = 255;
-                } else {
-                    binary[i] = binary[i+1] = binary[i+2] = 0;
-                    binary[i+3] = 255;
-                }
-            }
-            else {
-                binary[i] = binary[i + 1] = binary[i + 2] = grayscale;
-                binary[i + 3] = WHITE === grayscale ? 0 : data[i + 3];
-            }
-        }
-        else {
-            // 3 is alpha
-            if (false === opts.is_rgba) {
-                binary[binaryIndex] = data[i + 3] > 0 ? BLACK : WHITE;
-            } else {
-                for (j = 0; j < 3; j++) {
-                    binary[i + j] = 0 < data[i + j] ? WHITE : BLACK;
-                }
-
-                binary[i + 3] = data[i + 3] > 0 ? WHITE : BLACK;
-            }
-        }
+      binary[i] = grayscale;
+      binary[i + 1] = grayscale;
+      binary[i + 2] = grayscale;
+      binary[i + 3] = WHITE === grayscale ? 0 : data[i + 3];
     }
+  }
 
-    return binary;
-};
+  return binary;
+}
