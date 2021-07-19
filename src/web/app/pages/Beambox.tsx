ebcf62b5f67a4e12cc6691f1e97f5c72eda10ac0
Dean Sung <a71124725@gmail.com>
2021-07-19 14:33:13 +0800

    Fix TopBar import in beambox.tsx

--- src/web/app/pages/Beambox.tsx
+++ src/web/app/pages/Beambox.tsx
@@ -13,7 +13,7 @@ import SVGEditor from 'app/pages/svg-editor';
 import svgEditor from 'app/actions/beambox/svg-editor';
 import TimeEstimationButton from 'app/views/beambox/TimeEstimationButton/TimeEstimationButton';
 import { TimeEstimationButtonContextProvider } from 'app/views/beambox/TimeEstimationButton/TimeEstimationButtonContext';
-import { TopBar } from 'app/views/beambox/TopBar/TopBar';
+import TopBar from 'app/views/beambox/TopBar/TopBar';
 import { TopBarContextProvider } from 'app/views/beambox/TopBar/contexts/TopBarContext';
 import ZoomBlock from 'app/views/beambox/ZoomBlock/ZoomBlock';
 
