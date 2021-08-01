60538036929846c0747426bd8d9f12a80bcc39c8
Yanting Chen <mx.alexender@gmail.com>
2021-08-01 10:09:51 +0800

    add tests

---
+++ src/web/app/views/beambox/TopBar/ElementTitle.spec.tsx
@@ -0,0 +1,128 @@
+/* eslint-disable import/first */
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+jest.mock('helpers/i18n', () => ({
+  lang: {
+    topbar: {
+      tag_names: {
+        rect: 'Rectangle',
+        ellipse: 'Oval',
+        path: 'Path',
+        polygon: 'Polygon',
+        image: 'Image',
+        text: 'Text',
+        line: 'Line',
+        g: 'Group',
+        multi_select: 'Multiple Objects',
+        use: 'Imported Object',
+        svg: 'SVG Object',
+        dxf: 'DXF Object',
+      },
+    },
+  },
+}));
+
+const getSVGAsync = jest.fn();
+jest.mock('helpers/svg-editor-helper', () => ({
+  getSVGAsync,
+}));
+
+const getObjectLayer = jest.fn();
+getSVGAsync.mockImplementation((callback) => {
+  callback({
+    Canvas: {
+      getObjectLayer,
+    },
+  });
+});
+
+import ElementTitle from './ElementTitle';
+
+describe('should render correctly', () => {
+  it('no selected element', () => {
+    expect(toJson(shallow(<ElementTitle
+      selectedElem={null}
+    />))).toMatchSnapshot();
+    expect(getObjectLayer).not.toHaveBeenCalled();
+  });
+
+  it('multiple selections', () => {
+    document.body.innerHTML = '<g id="svg_1" data-tempgroup="true" />';
+    const wrapper = shallow(<ElementTitle
+      selectedElem={document.getElementById('svg_1')}
+    />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(getObjectLayer).not.toHaveBeenCalled();
+  });
+
+  describe('single selection', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('not use', () => {
+      getObjectLayer.mockReturnValue({
+        title: 'Layer 1',
+      });
+      document.body.innerHTML = '<rect id="svg_1" />';
+      const wrapper = shallow(<ElementTitle
+        selectedElem={document.getElementById('svg_1')}
+      />);
+      expect(toJson(wrapper)).toMatchSnapshot();
+      expect(getObjectLayer).toHaveBeenCalledTimes(1);
+      expect(getObjectLayer).toHaveBeenNthCalledWith(1, document.getElementById('svg_1'));
+    });
+
+    it('svg', () => {
+      getObjectLayer.mockReturnValue({
+        title: 'Layer 1',
+      });
+      document.body.innerHTML = '<use id="svg_1" data-svg="true" />';
+      const wrapper = shallow(<ElementTitle
+        selectedElem={document.getElementById('svg_1')}
+      />);
+      expect(toJson(wrapper)).toMatchSnapshot();
+      expect(getObjectLayer).toHaveBeenCalledTimes(1);
+      expect(getObjectLayer).toHaveBeenNthCalledWith(1, document.getElementById('svg_1'));
+    });
+
+    it('dxf', () => {
+      getObjectLayer.mockReturnValue({
+        title: 'Layer 1',
+      });
+      document.body.innerHTML = '<use id="svg_1" data-dxf="true" />';
+      const wrapper = shallow(<ElementTitle
+        selectedElem={document.getElementById('svg_1')}
+      />);
+      expect(toJson(wrapper)).toMatchSnapshot();
+      expect(getObjectLayer).toHaveBeenCalledTimes(1);
+      expect(getObjectLayer).toHaveBeenNthCalledWith(1, document.getElementById('svg_1'));
+    });
+
+    it('imported object', () => {
+      getObjectLayer.mockReturnValue({
+        title: 'Layer 1',
+      });
+      document.body.innerHTML = '<use id="svg_1" />';
+      const wrapper = shallow(<ElementTitle
+        selectedElem={document.getElementById('svg_1')}
+      />);
+      expect(toJson(wrapper)).toMatchSnapshot();
+      expect(getObjectLayer).toHaveBeenCalledTimes(1);
+      expect(getObjectLayer).toHaveBeenNthCalledWith(1, document.getElementById('svg_1'));
+    });
+
+    it('no layer title given', () => {
+      getObjectLayer.mockReturnValue(null);
+      document.body.innerHTML = '<use id="svg_1" />';
+      const wrapper = shallow(<ElementTitle
+        selectedElem={document.getElementById('svg_1')}
+      />);
+      expect(toJson(wrapper)).toMatchSnapshot();
+      expect(getObjectLayer).toHaveBeenCalledTimes(1);
+      expect(getObjectLayer).toHaveBeenNthCalledWith(1, document.getElementById('svg_1'));
+    });
+  });
+});
