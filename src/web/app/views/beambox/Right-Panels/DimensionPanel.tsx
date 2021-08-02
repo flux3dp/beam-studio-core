1f42f5af5d1f97b49cbe2bd44ba6d99ecdba83c8
Yanting Chen <ChenYanting@users.noreply.github.com>
2021-08-02 12:38:38 +0800

    add tests (#190)
    * add tests
    
    * add tests
    
    * add tests
    
    * add tests

--- src/web/app/views/beambox/Right-Panels/DimensionPanel.tsx
+++ src/web/app/views/beambox/Right-Panels/DimensionPanel.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 import Constant from 'app/actions/beambox/constant';
-import history from 'app/svgedit/history';
 import i18n from 'helpers/i18n';
+import HistoryCommandFactory from 'app/svgedit/HistoryCommandFactory';
 import KeycodeConstants from 'app/constants/keycode-constants';
 import storage from 'implementations/storage';
 import SymbolMaker from 'helpers/symbol-maker';
@@ -23,6 +23,7 @@ const panelMap = {
   ellipse: ['cx', 'cy', 'rot', 'rx', 'ry', 'lock'],
   line: ['x1', 'y1', 'rot', 'x2', 'y2'],
   image: ['x', 'y', 'rot', 'w', 'h', 'lock'],
+  img: ['x', 'y', 'rot', 'w', 'h', 'lock'],
   text: ['x', 'y', 'rot', 'w', 'h', 'lock'],
   use: ['x', 'y', 'rot', 'w', 'h', 'lock'],
 };
@@ -114,7 +115,7 @@ class DimensionPanel extends React.Component<Props> {
   };
 
   handleSizeChange = (type:string, val:number): void => {
-    const batchCmd = new history.BatchCommand('Object Panel Size Change');
+    const batchCmd = HistoryCommandFactory.createBatchCommand('Object Panel Size Change');
     const { updateDimensionValues, getDimensionValues } = this.props;
     const response = {
       dimensionValues: {} as any,
@@ -194,7 +195,6 @@ class DimensionPanel extends React.Component<Props> {
     getDimensionValues(response);
     const dimensionValues = response.dimensionValues;
     const isRatioFixed = dimensionValues.isRatioFixed || false;
-
     switch (type) {
       case 'x':
         return (
@@ -411,7 +411,7 @@ class DimensionPanel extends React.Component<Props> {
     const { elem } = this.props;
     let panels = ['x', 'y', 'rot', 'w', 'h'];
     if (elem) {
-      panels = panelMap[elem.tagName] || ['x', 'y', 'rot', 'w', 'h'];
+      panels = panelMap[elem.tagName.toLowerCase()] || ['x', 'y', 'rot', 'w', 'h'];
     }
     return (
       <div className="dimension-panel">
