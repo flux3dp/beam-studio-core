409062c360a8d223b14e45a5ec2e7ac9b9d65294
Yanting Chen <mx.alexender@gmail.com>
2021-08-12 23:37:35 +0800

    add tests

--- src/web/app/views/beambox/PathPreview/SidePanel.tsx
+++ src/web/app/views/beambox/PathPreview/SidePanel.tsx
@@ -15,50 +15,50 @@ interface Props {
   togglePathPreview: () => void;
 }
 
-class SidePanel extends React.PureComponent<Props> {
-  // eslint-disable-next-line class-methods-use-this
-  renderDataBlock(label: string, value: string): JSX.Element {
-    return (
-      <div className="data-block">
-        <div className="item">{label}</div>
-        <div className="value">{value}</div>
-      </div>
-    );
-  }
+function SidePanel({
+  size,
+  estTime,
+  lightTime,
+  rapidTime,
+  cutDist,
+  rapidDist,
+  currentPosition,
+  handleStartHere,
+  togglePathPreview,
+}: Props): JSX.Element {
+  const LANG = i18n.lang.beambox.path_preview;
+  const renderDataBlock = (label: string, value: string): JSX.Element => (
+    <div className="data-block">
+      <div className="item">{label}</div>
+      <div className="value">{value}</div>
+    </div>
+  );
 
-  render(): JSX.Element {
-    const className = classNames({ win: window.os === 'Windows' });
-    const {
-      size, estTime, lightTime, rapidTime, cutDist, rapidDist, currentPosition,
-      handleStartHere, togglePathPreview,
-    } = this.props;
-    const LANG = i18n.lang.beambox.path_preview;
-    return (
-      <div id="path-preview-side-panel" className={className}>
-        <div className="title">{LANG.preview_info}</div>
-        <div className="datas">
-          {this.renderDataBlock(LANG.size, size)}
-          {this.renderDataBlock(LANG.estimated_time, estTime)}
-          {this.renderDataBlock(LANG.cut_time, lightTime)}
-          {this.renderDataBlock(LANG.rapid_time, rapidTime)}
-          {this.renderDataBlock(LANG.cut_distance, cutDist)}
-          {this.renderDataBlock(LANG.rapid_distance, rapidDist)}
-          {this.renderDataBlock(LANG.current_position, currentPosition)}
+  return (
+    <div id="path-preview-side-panel" className={classNames({ win: window.os === 'Windows' })}>
+      <div className="title">{LANG.preview_info}</div>
+      <div className="datas">
+        {renderDataBlock(LANG.size, size)}
+        {renderDataBlock(LANG.estimated_time, estTime)}
+        {renderDataBlock(LANG.cut_time, lightTime)}
+        {renderDataBlock(LANG.rapid_time, rapidTime)}
+        {renderDataBlock(LANG.cut_distance, cutDist)}
+        {renderDataBlock(LANG.rapid_distance, rapidDist)}
+        {renderDataBlock(LANG.current_position, currentPosition)}
+      </div>
+      <div className="remark">
+        {LANG.remark}
+      </div>
+      <div className="buttons">
+        <div className="btn btn-default primary" onClick={handleStartHere}>
+          {LANG.start_here}
         </div>
-        <div className="remark">
-          {LANG.remark}
-        </div>
-        <div className="buttons">
-          <div className="btn btn-default primary" onClick={handleStartHere}>
-            {LANG.start_here}
-          </div>
-          <div className="btn btn-default" onClick={togglePathPreview}>
-            {LANG.end_preview}
-          </div>
+        <div className="btn btn-default" onClick={togglePathPreview}>
+          {LANG.end_preview}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SidePanel;
