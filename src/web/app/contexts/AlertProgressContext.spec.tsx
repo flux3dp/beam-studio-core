fc612f3deec3b2ce277afc4a997802880dee9f89
Yanting Chen <mx.alexender@gmail.com>
2021-08-16 14:41:44 +0800

    test

--- src/web/app/contexts/AlertProgressContext.spec.tsx
+++ src/web/app/contexts/AlertProgressContext.spec.tsx
@@ -41,7 +41,7 @@ test('should render correctly', () => {
     </AlertProgressContextProvider>,
   );
   expect(toJson(wrapper)).toMatchSnapshot();
-  expect(eventEmitter.eventNames().length).toBe(6);
+  expect(eventEmitter.eventNames().length).toBe(5);
 
   eventEmitter.emit('OPEN_PROGRESS', {
     id: 'check-status',
