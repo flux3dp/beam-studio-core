13f449c2cfddc2192cee25a1bcbd3ecd3fac49c6
Jess <jess@flux3dp.com>
2021-09-01 11:47:32 +0800

    add jest files

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    preset: "ts-jest",
+    testMatch: [
+      "**/?(*.)+(spec|test).(ts|tsx)"
+    ],
+    collectCoverageFrom: [
+      "src/**/*.(ts|tsx)",
+      "!src/web/setupTests.ts",
+      "!src/web/app/svgedit/**",
+      "!src/web/app/actions/beambox/svg-editor.ts",
+      "!src/web/app/lang/**",
+      "!src/implementations/**",
+      "!src/web/app/constants/**",
+    ],
+    globals: {
+      "ts-jest": {
+        tsconfig: "src/tsconfig.json"
+      }
+    },
+    setupFilesAfterEnv: ["./src/web/setupTests.ts"],
+    moduleDirectories: ["node_modules", "src/web", "src"],
+    coverageReporters: ["text-summary", "html"]
+  }
