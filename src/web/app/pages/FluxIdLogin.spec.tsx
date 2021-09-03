539444c495592b0094da2ecea128734c1fb05388
Dean Sung <a71124725@gmail.com>
2021-09-03 10:32:39 +0800

    Update tests

--- src/web/app/pages/FluxIdLogin.spec.tsx
+++ src/web/app/pages/FluxIdLogin.spec.tsx
@@ -3,8 +3,10 @@ import * as React from 'react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
+const mockClearAllDialogComponents = jest.fn();
 const mockShowLoginDialog = jest.fn();
 jest.mock('app/actions/dialog-caller', () => ({
+  clearAllDialogComponents: mockClearAllDialogComponents,
   showLoginDialog: mockShowLoginDialog,
 }));
 
@@ -13,5 +15,6 @@ import FluxIdLogin from './FluxIdLogin';
 test('should render correctly', () => {
   const wrapper = mount(<FluxIdLogin />);
   expect(toJson(wrapper)).toMatchSnapshot();
+  expect(mockClearAllDialogComponents).toHaveBeenCalledTimes(1);
   expect(mockShowLoginDialog).toHaveBeenCalledTimes(1);
 });
--- src/web/helpers/social-auth.spec.ts
+++ src/web/helpers/social-auth.spec.ts
@@ -1,3 +1,10 @@
+const mockGet = jest.fn();
+
+jest.mock('implementations/storage', () => ({
+  get: mockGet,
+}));
+
+// eslint-disable-next-line import/first
 import socialAuth from './social-auth';
 
 describe('test social-auth', () => {
@@ -8,7 +15,34 @@ describe('test social-auth', () => {
   });
 
   describe('result is true', () => {
-    test('opener\'s location is at flux id login page', () => {
+    test('opener\'s location is at flux id login page and Beam Studio is not ready', () => {
+      window.opener.location.hash = '#/initialize/connect/flux-id-login';
+      window.opener.dispatchEvent = jest.fn();
+      window.close = jest.fn();
+      mockGet.mockReturnValueOnce(false);
+      socialAuth(true);
+
+      expect(mockGet).toHaveBeenNthCalledWith(1, 'printer-is-ready');
+      expect(window.opener.location.hash).toBe('#initialize/connect/select-connection-type');
+      expect(window.opener.dispatchEvent).not.toHaveBeenCalled();
+      expect(window.close).toHaveBeenCalledTimes(1);
+    });
+
+    test('opener\'s location is at flux id login page and Beam Studio is ready', () => {
+      window.opener.location.hash = '#/initialize/connect/flux-id-login';
+      window.opener.dispatchEvent = jest.fn();
+      window.close = jest.fn();
+      mockGet.mockReturnValueOnce(true);
+      socialAuth(true);
+
+      expect(mockGet).toHaveBeenNthCalledWith(1, 'printer-is-ready');
+      expect(window.opener.location.hash).toBe('#studio/beambox');
+      expect(window.opener.dispatchEvent).toHaveBeenCalledTimes(1);
+      expect(window.close).toHaveBeenCalledTimes(1);
+    });
+
+    test('opener\'s location is at flux id login page and printer is not ready', () => {
+      mockGet.mockReturnValueOnce(false);
       window.opener.location.hash = '#/initialize/connect/flux-id-login';
       window.opener.dispatchEvent = jest.fn();
       window.close = jest.fn();
