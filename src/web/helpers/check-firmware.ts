e77e58faea866fd21da1e79f337e6e46d49cc804
Dean Sung <a71124725@gmail.com>
2021-09-16 13:47:41 +0800

    Refactor check firmware

--- src/web/helpers/check-firmware.ts
+++ src/web/helpers/check-firmware.ts
@@ -44,54 +44,52 @@ function checkMachineSeries(model) {
  * @return Promise
  */
 
-export default function (device: IDeviceInfo, type: string) {
-  const deferred = $.Deferred();
-  // return deferred.reject if network is unavailable.
+export default async function checkFirmware(
+  device: IDeviceInfo,
+  type: string,
+): Promise<{ needUpdate: boolean, [key: string]: string | boolean }> {
   if (!navigator.onLine) {
-    deferred.reject({
-      needUpdate: true,
-    });
-    return deferred.promise();
+    throw new Error('Offline');
   }
 
-  const series = checkMachineSeries(device.model);
-  const info = infoMap[series][type];
-  const requestData = {
-    feature: 'check_update',
-    key: info.api_key,
-  };
+  return new Promise<{ needUpdate: boolean, [key: string]: string | boolean }>((resolve) => {
+    const series = checkMachineSeries(device.model);
+    const info = infoMap[series][type];
+    const requestData = {
+      feature: 'check_update',
+      key: info.api_key,
+    };
 
-  $.ajax({
-    url: 'https://flux3dp.com/api_entry/',
-    data: requestData,
-  })
-    .done((response) => {
-      // eslint-disable-next-line no-console
-      console.log(response);
-      try {
-        response.needUpdate = versionCompare(device.version, response.latest_version);
+    $.ajax({
+      url: 'https://flux3dp.com/api_entry/',
+      data: requestData,
+    })
+      .done((response) => {
         // eslint-disable-next-line no-console
-        console.log('response.needUpdate: ', response.needUpdate);
-        response.latestVersion = response.latest_version;
-        if (response.changelog_en) {
-          response.changelog_en = response.changelog_en.replace(/[\r]/g, '<br/>');
+        console.log(response);
+        try {
+          response.needUpdate = versionCompare(device.version, response.latest_version);
+          // eslint-disable-next-line no-console
+          console.log('response.needUpdate: ', response.needUpdate);
+          response.latestVersion = response.latest_version;
+          if (response.changelog_en) {
+            response.changelog_en = response.changelog_en.replace(/[\r]/g, '<br/>');
+          }
+          if (response.changelog_zh) {
+            response.changelog_zh = response.changelog_zh.replace(/[\r]/g, '<br/>');
+          }
+          response.downloadUrl = info.downloadUrl.replace('[version]', response.latest_version);
+          resolve(response);
+        } catch {
+          resolve({
+            needUpdate: false,
+          });
         }
-        if (response.changelog_zh) {
-          response.changelog_zh = response.changelog_zh.replace(/[\r]/g, '<br/>');
-        }
-        response.downloadUrl = info.downloadUrl.replace('[version]', response.latest_version);
-        deferred.resolve(response);
-      } catch {
-        deferred.resolve({
+      })
+      .fail(() => {
+        resolve({
           needUpdate: false,
         });
-      }
-    })
-    .fail(() => {
-      deferred.resolve({
-        needUpdate: false,
       });
-    });
-
-  return deferred.promise();
+  });
 }
