f7949c8f090125c353bf53210ed9db0df8949e29
Simon Ko <simon@flux3dp.com>
2023-01-20 00:28:15 +0800

    Update SVGCanvas definitions

--- src/web/interfaces/ISVGDrawing.d.ts
+++ src/web/interfaces/ISVGDrawing.d.ts
@@ -1,5 +1,8 @@
 export default interface ISVGDrawing {
   getCurrentLayer: () => SVGGElement | null;
   getCurrentLayerName: () => string | null;
+  getLayerName: (index: number) => string | null;
+  getNumLayers: () => number;
+  setLayerOpacity: (name: string, opacity: number) => void;
   releaseId: (id: string) => void;
 }
