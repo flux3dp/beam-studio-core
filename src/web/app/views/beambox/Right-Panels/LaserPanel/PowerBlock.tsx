ccd8ea64d3aa4e9233be1fb96dc5c6a743f6af6e
Dean Sung <dean@flux3dp.com>
2023-03-06 12:08:24 +0800

    Extract Power and Speed Block from LaserPanel

---
+++ src/web/app/views/beambox/Right-Panels/LaserPanel/PowerBlock.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import UnitInput from 'app/widgets/Unit-Input-v2';
+import useI18n from 'helpers/useI18n';
+
+import styles from './PowerBlock.module.scss';
+
+interface Props {
+  power: number;
+  hasMultipleValue: boolean;
+  onChange: (val: number) => void;
+}
+
+const MAX_VALUE = 100;
+const MIN_VALUE = 1;
+
+function PowerBlock({ power, hasMultipleValue, onChange }: Props): JSX.Element {
+  const lang = useI18n();
+  const t = lang.beambox.right_panel.laser_panel;
+
+  return (
+    <div className={styles.panel}>
+      <span className={styles.title}>{t.strength}</span>
+      <UnitInput
+        id="power"
+        className={{ [styles.input]: true }}
+        min={MIN_VALUE}
+        max={MAX_VALUE}
+        unit="%"
+        defaultValue={power}
+        getValue={onChange}
+        decimal={1}
+        displayMultiValue={hasMultipleValue}
+      />
+      <input
+        id="power_value"
+        type="range"
+        min={MIN_VALUE}
+        max={MAX_VALUE}
+        step={1}
+        value={power}
+        onChange={(e) => onChange(parseInt(e.target.value, 10))}
+      />
+      {power < 10 && (
+        <div className={styles.warning}>
+          <div className={styles['warning-icon']}>!</div>
+          <div className={styles['warning-text']}>{t.low_power_warning}</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PowerBlock;
