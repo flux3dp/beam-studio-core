0a4c2b693ed92a3a5d33a6bb5d46ca287ce16b3b
Dean Sung <dean@flux3dp.com>
2023-03-23 10:09:46 +0800

    Fix breadcrumbs

--- src/web/app/views/monitor/widgets/Breadcrumbs.tsx
+++ src/web/app/views/monitor/widgets/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, memo, ReactNode, useContext } from 'react';
+import React, { memo, ReactNode, useContext } from 'react';
 import { Breadcrumb } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
 
@@ -8,22 +8,16 @@ import styles from './Breadcrumbs.module.scss';
 
 const Breadcrumbs = (): JSX.Element => {
   const { currentPath, onSelectFolder } = useContext(MonitorContext);
-  const breadcrumbItems: { title: ReactNode, onClick: () => void }[] = [
-    { title: <HomeOutlined />, onClick: () => onSelectFolder('', true) }
+  const breadcrumbItems: { title: ReactNode; onClick: () => void; className: string; }[] = [
+    { title: <HomeOutlined />, onClick: () => onSelectFolder('', true), className: styles.item }
   ];
   currentPath.forEach((folder, i) => {
     const handleClick = () => onSelectFolder(currentPath.slice(0, i + 1).join('/'), true);
-    breadcrumbItems.push({ title: folder, onClick: handleClick });
+    breadcrumbItems.push({ title: folder, onClick: handleClick, className: styles.item });
   });
 
-  // Somehow onClick in items is not used by antd, write a custom itemRender to add event
-  const itemRender = (item: { title: ReactNode, onClick: (e: MouseEvent) => void }) => {
-    const { title, onClick } = item;
-    return <span className={styles.item} onClick={onClick}>{title}</span>;
-  };
-
   return (
-    <Breadcrumb itemRender={itemRender} items={breadcrumbItems} />
+    <Breadcrumb items={breadcrumbItems} />
   );
 };
 
