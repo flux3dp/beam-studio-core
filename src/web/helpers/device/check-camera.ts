3c246f0e652220bc8118854b339b5f12cecb7cb6
Dean Sung <dean@flux3dp.com>
2023-03-23 18:12:21 +0800

    Refactor connect machine ip page (#324)
    * Refactor ConnectMachineIp Page
    
    * Remove testing console

---
+++ src/web/helpers/device/check-camera.ts
@@ -0,0 +1,20 @@
+import deviceMaster from 'helpers/device-master';
+import { IDeviceInfo } from 'interfaces/IDevice';
+
+const checkCamera = async (device: IDeviceInfo): Promise<boolean> => {
+  try {
+    const selectResult = await deviceMaster.select(device);
+    if (!selectResult.success) {
+      throw new Error(selectResult.error || 'Fail to select device');
+    }
+    await deviceMaster.connectCamera();
+    await deviceMaster.takeOnePicture();
+    deviceMaster.disconnectCamera();
+    return true;
+  } catch (e) {
+    console.log(e);
+  }
+  return false;
+};
+
+export default checkCamera;
