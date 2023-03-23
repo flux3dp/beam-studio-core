b335cade36bec45429c17bfe75c7fa352a724350
Dean Sung <dean@flux3dp.com>
2023-03-23 18:00:14 +0800

    Remove testing console

--- src/web/helpers/device/check-camera.ts
+++ src/web/helpers/device/check-camera.ts
@@ -3,7 +3,6 @@ import { IDeviceInfo } from 'interfaces/IDevice';
 
 const checkCamera = async (device: IDeviceInfo): Promise<boolean> => {
   try {
-    console.log(device);
     const selectResult = await deviceMaster.select(device);
     if (!selectResult.success) {
       throw new Error(selectResult.error || 'Fail to select device');
