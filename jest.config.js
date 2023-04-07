f4f9bbdf6f414c03f8774d112bb77c282b157b9e
Dean Sung <dean@flux3dp.com>
2023-04-07 14:50:52 +0800

    Fix failed to collect code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  preset: "ts-jest",
+  preset: 'ts-jest',
   testMatch: [
-    "**/?(*.)+(spec|test).(ts|tsx)"
+    '**/?(*.)+(spec|test).(ts|tsx)'
   ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'src/tsconfig.json' }],
+  },
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
   collectCoverageFrom: [
-    "src/**/*.(ts|tsx)",
-    "!src/web/setupTests.ts",
-    "!src/web/app/svgedit/**",
-    "!src/web/app/actions/beambox/svg-editor.ts",
-    "!src/web/app/lang/**",
-    "!src/implementations/**",
-    "!src/web/app/constants/**",
+    'src/**/*.(ts|tsx)',
+    '!src/web/setupTests.ts',
+    '!src/web/app/svgedit/**',
+    '!src/web/app/actions/beambox/svg-editor.ts',
+    '!src/web/app/lang/**',
+    '!src/implementations/**',
+    '!src/web/app/constants/**',
+    // TODO: write unit test for symbol-maker and remove below line
+    '!src/web/helpers/symbol-maker.ts',
+    '!src/**/*.worker.ts',
   ],
-  globals: {
-    "ts-jest": {
-      tsconfig: "src/tsconfig.json"
-    }
-  },
-  setupFilesAfterEnv: ["./src/web/setupTests.ts"],
-  moduleDirectories: ["node_modules", "src/web", "src"],
-  coverageReporters: ["text-summary", "html"]
+  setupFilesAfterEnv: ['./src/web/setupTests.ts'],
+  moduleDirectories: ['node_modules', 'src/web', 'src'],
+  coverageReporters: ['text-summary', 'html']
 }
