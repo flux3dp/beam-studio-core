cb7d2f038324f32a4879f08c685e888d5989a8dc
Dean Sung <dean@flux3dp.com>
2023-06-19 15:10:43 +0800

    Remove unused import

--- src/web/app/components/dialogs/camera/FishEyeCalibration/Align.spec.tsx
+++ src/web/app/components/dialogs/camera/FishEyeCalibration/Align.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 
 import Align from './Align';
 
