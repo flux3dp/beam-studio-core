1f2eeeb5bb252f1da6d5fb478c2a74fab5b0505d
Dean Sung <dean@flux3dp.com>
2023-07-05 17:54:21 +0800

    Fix test

--- src/web/app/views/beambox/Right-Panels/ConfigPanel/ConfigPanelContext.spec.tsx
+++ src/web/app/views/beambox/Right-Panels/ConfigPanel/ConfigPanelContext.spec.tsx
@@ -1,16 +1,36 @@
 import { Action, getDefaultState, reducer } from './ConfigPanelContext';
 
+enum DataType {
+  speed = 'speed',
+  strength = 'strength',
+  repeat = 'repeat',
+  configName = 'configName',
+}
+
+jest.mock('helpers/layer/layer-config-helper', () => ({
+  DataType: {
+    speed: 'speed',
+    strength: 'strength',
+    repeat: 'repeat',
+    zstep: 'zstep',
+    configName: 'configName',
+  },
+  defaultConfig: {
+    speed: 20,
+    strength: 15,
+    repeat: 1,
+    zstep: 0,
+    configName: '',
+  },
+}));
+
 describe('test ConfigPanelContext', () => {
   test('getDefaultState should work', () => {
     expect(getDefaultState()).toEqual({
-      speed: { value: 3 },
-      power: { value: 1 },
-      ink: { value: 3 },
+      speed: { value: 20 },
+      power: { value: 15 },
       repeat: { value: 1 },
-      height: { value: -3 },
       zStep: { value: 0 },
-      diode: { value: 0 },
-      type: { value: 1 },
       configName: { value: '' },
     });
   });
@@ -26,13 +46,9 @@ describe('test ConfigPanelContext', () => {
     } as Action);
     expect(newState).toEqual({
       speed: { value: 2, hasMultiValue: true },
-      power: { value: 1 },
-      ink: { value: 3 },
+      power: { value: 15 },
       repeat: { value: 2 },
-      height: { value: -3 },
       zStep: { value: 0 },
-      diode: { value: 0 },
-      type: { value: 1 },
       configName: { value: '' },
     });
   });
@@ -48,13 +64,9 @@ describe('test ConfigPanelContext', () => {
     } as Action);
     expect(newState).toEqual({
       speed: { value: 2 },
-      power: { value: 1 },
-      ink: { value: 3 },
+      power: { value: 15 },
       repeat: { value: 2 },
-      height: { value: -3 },
       zStep: { value: 0 },
-      diode: { value: 0 },
-      type: { value: 1 },
       configName: { value: '' },
     });
   });
@@ -66,14 +78,10 @@ describe('test ConfigPanelContext', () => {
       payload: 'test',
     } as Action);
     expect(newState).toEqual({
-      speed: { value: 3 },
-      power: { value: 1 },
-      ink: { value: 3 },
+      speed: { value: 20 },
+      power: { value: 15 },
       repeat: { value: 1 },
-      height: { value: -3 },
       zStep: { value: 0 },
-      diode: { value: 0 },
-      type: { value: 1 },
       configName: { value: 'test' },
       selectedItem: 'test',
     });
