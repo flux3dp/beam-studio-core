41994908242bade4ec2f253c20ccaf6bd783c50a
Dean Sung <dean@flux3dp.com>
2023-07-19 16:47:23 +0800

    Add prespray fcode generation

--- src/web/app/actions/beambox/prespray-area.ts
+++ src/web/app/actions/beambox/prespray-area.ts
@@ -6,10 +6,25 @@ let presprayAreaRect: SVGRectElement;
 const areaWidth = 200;
 const areaHeight = 200;
 
+const round = (num: number, decimal: number): number => {
+  const factor = 10 ** decimal;
+  return Math.round(num * factor) / factor;
+};
+
 const togglePresprayArea = (): void => {
   const shouldShow = document.querySelectorAll('g.layer[data-module="2"]:not([display="none"]').length > 0;
   if (shouldShow) presprayAreaRect.removeAttribute('display');
   else presprayAreaRect.setAttribute('display', 'none');
+};
+
+const getPosition = (mm = false): { x: number; y: number; w: number; h: number; } => {
+  const pxX = parseInt(presprayAreaRect?.getAttribute('x'), 10);
+  const pxY = parseInt(presprayAreaRect?.getAttribute('y'), 10);
+  const pxW = areaWidth;
+  const pxH = areaHeight;
+  if (!mm) return { x: pxX, y: pxY, w: pxW, h: pxH };
+  const { dpmm } = constant;
+  return { x: round(pxX / dpmm, 2), y: round(pxY / dpmm, 2), w: round(pxW / dpmm, 2), h: round(pxH / dpmm, 2) };
 };
 
 const generatePresprayArea = (): void => {
@@ -38,8 +53,9 @@ let startY = 0;
 let workareaSize = { w: 0, h: 0 };
 
 const startDrag = (): void => {
-  startX = parseInt(presprayAreaRect?.getAttribute('x'), 10);
-  startY = parseInt(presprayAreaRect?.getAttribute('y'), 10);
+  const { x, y } = getPosition();
+  startX = x;
+  startY = y;
   const workarea = beamboxPreference.read('workarea');
   workareaSize = {
     w: constant.dimension.getWidth(workarea),
@@ -61,6 +77,7 @@ export default {
   checkMouseTarget,
   drag,
   generatePresprayArea,
+  getPosition,
   startDrag,
   togglePresprayArea,
 };
