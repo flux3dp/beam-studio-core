33651339af06574625ce8d98d958f5fe64af4087
Dean Sung <dean@flux3dp.com>
2023-08-10 12:07:03 +0800

    Add presets for 1064 2w ir laser

--- src/web/app/constants/layer-module/layer-modules.ts
+++ src/web/app/constants/layer-module/layer-modules.ts
@@ -1,7 +1,7 @@
 // ref: https://www.notion.so/flux3dp/030619b6721849498cadc57e660107d3?pvs=4
 
 enum LayerModule {
-  LASER_10W_DIODE = 1, // LAER_10W_DIODE
+  LASER_10W_DIODE = 1, // or default CO2 laser for BeamSeries
   LASER_20W_DIODE = 2,
   LASER_1064 = 4,
   PRINTER = 5,
