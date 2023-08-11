c01124096ec8ca9a23ab9cd3492926355de40f16
Dean Sung <dean@flux3dp.com>
2023-08-11 10:48:10 +0800

    Fix monitor tab

--- src/web/app/views/monitor/Monitor.tsx
+++ src/web/app/views/monitor/Monitor.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Modal, Tabs } from 'antd';
 import { CameraOutlined, FolderOutlined, PictureOutlined } from '@ant-design/icons';
 
+import deviceConstants from 'app/constants/device-constants';
 import MonitorStatus from 'helpers/monitor-status';
 import useI18n from 'helpers/useI18n';
 import { Mode } from 'app/constants/monitor-constants';
@@ -15,7 +16,6 @@ import MonitorTabExtraContent from './MonitorTabExtraContent';
 import MonitorTask from './MonitorTask';
 
 interface Props {
-  mode?: string;
   device: IDeviceInfo
 }
 
@@ -23,6 +23,8 @@ const Monitor = (props: Props): JSX.Element => {
   const { device } = props;
   const { currentPath, mode, onClose, report, setMonitorMode, taskImageURL } = useContext(MonitorContext);
   const LANG = useI18n();
+  const taskMode = report.st_id === deviceConstants.status.IDLE ? Mode.PREVIEW : Mode.WORKING
+  const monitorMode = [Mode.PREVIEW, Mode.FILE_PREVIEW, Mode.WORKING].includes(mode) ? taskMode : mode;
 
   // const renderRelocate = (): JSX.Element => {
   //   return (
@@ -32,68 +34,62 @@ const Monitor = (props: Props): JSX.Element => {
   //   );
   // };
 
-  const render = (): JSX.Element => {
-    const monitorMode = [Mode.PREVIEW, Mode.FILE_PREVIEW, Mode.WORKING].includes(mode)
-      ? Mode.PREVIEW : mode;
-
-    const tabItems = [
+  const tabItems = [
+    {
+      label: (
+        <div>
+          <FolderOutlined />
+          {LANG.topmenu.file.label}
+        </div>
+      ),
+      key: Mode.FILE,
+      children: <MonitorFilelist path={currentPath.join('/')} />,
+    },
+    {
+      label: (
+        <div>
+          <CameraOutlined />
+          {LANG.monitor.camera}
+        </div>
+      ),
+      key: Mode.CAMERA,
+      children: <MonitorCamera device={device} />,
+    },
+  ];
+  if (taskImageURL) {
+    tabItems.unshift(
       {
         label: (
           <div>
-            <FolderOutlined />
-            {LANG.topmenu.file.label}
+            <PictureOutlined />
+            {LANG.monitor.taskTab}
           </div>
         ),
-        key: Mode.FILE,
-        children: <MonitorFilelist path={currentPath.join('/')} />,
-      },
-      {
-        label: (
+        key: taskMode,
+        children: (
           <div>
-            <CameraOutlined />
-            {LANG.monitor.camera}
+            <MonitorTask />
           </div>
         ),
-        key: Mode.CAMERA,
-        children: <MonitorCamera device={device} />,
       },
-    ];
-    if (taskImageURL) {
-      tabItems.unshift(
-        {
-          label: (
-            <div>
-              <PictureOutlined />
-              {LANG.monitor.taskTab}
-            </div>
-          ),
-          key: Mode.PREVIEW,
-          children: (
-            <div>
-              <MonitorTask />
-            </div>
-          ),
-        },
-      );
-    }
-    return (
-      <Modal
-        open
-        centered
-        onCancel={onClose}
-        title={`${device.name} - ${report ? MonitorStatus.getDisplayStatus(report.st_label) : LANG.monitor.connecting}`}
-        footer={null}
-      >
-        <Tabs
-          activeKey={monitorMode}
-          items={tabItems}
-          onChange={(key: Mode) => setMonitorMode(key)}
-          tabBarExtraContent={<MonitorTabExtraContent />}
-        />
-      </Modal>
     );
-  };
-  return render();
+  }
+  return (
+    <Modal
+      open
+      centered
+      onCancel={onClose}
+      title={`${device.name} - ${report ? MonitorStatus.getDisplayStatus(report.st_label) : LANG.monitor.connecting}`}
+      footer={null}
+    >
+      <Tabs
+        activeKey={monitorMode}
+        items={tabItems}
+        onChange={(key: Mode) => setMonitorMode(key)}
+        tabBarExtraContent={<MonitorTabExtraContent />}
+      />
+    </Modal>
+  );
 };
 
 export default Monitor;
