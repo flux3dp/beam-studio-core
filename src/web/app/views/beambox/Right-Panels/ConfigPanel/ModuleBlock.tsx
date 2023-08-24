2ed691a1678ff2606c30ad0d0dfc08bd579de2fc
Dean Sung <dean@flux3dp.com>
2023-08-24 09:58:22 +0800

    Add 20w laser option in ModuleBlock

--- src/web/app/views/beambox/Right-Panels/ConfigPanel/ModuleBlock.tsx
+++ src/web/app/views/beambox/Right-Panels/ConfigPanel/ModuleBlock.tsx
@@ -93,6 +93,7 @@ const ModuleBlock = (): JSX.Element => {
     { label: lang.layer_module.laser_10w_diode, value: LayerModule.LASER_10W_DIODE },
     { label: lang.layer_module.printing, value: LayerModule.PRINTER },
     { label: lang.layer_module.laser_2w_infrared, value: LayerModule.LASER_1064 },
+    { label: lang.layer_module.laser_20w_diode, value: LayerModule.LASER_20W_DIODE },
   ];
 
   return (
