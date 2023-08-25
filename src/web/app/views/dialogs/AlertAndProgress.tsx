7cdd6236214753a7393b4090d534f8466d7e99dc
Dean Sung <dean@flux3dp.com>
2023-08-25 14:50:18 +0800

    Change spinner (#375)
    * Use proper spinner in loading screen (for web version)
    
    * Fix spinning dialog vertical center
    
    * Remove unused codes
    
    * Remove deprecated <center> tag
    
    ---------
    
    Co-authored-by: Simon Ko <simon@flux3dp.com>

--- src/web/app/views/dialogs/AlertAndProgress.tsx
+++ src/web/app/views/dialogs/AlertAndProgress.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { Modal, Progress } from 'antd';
+import { SpinLoading } from 'antd-mobile';
 
 import Alert from 'app/views/dialogs/Alert';
 import browser from 'implementations/browser';
 import i18n from 'helpers/i18n';
-import LoadingGridAnimation from 'app/widgets/LoadingGridAnimation';
 import ProgressConstants from 'app/constants/progress-constants';
 import { AlertProgressContext } from 'app/contexts/AlertProgressContext';
 import { IAlert } from 'interfaces/IAlert';
@@ -46,75 +46,83 @@ const AlertsAndProgress = (): JSX.Element => {
       }
     }
   });
-
   if (alertProgressStack.length === 0) return <div />;
+
+  const renderNonstop = (data: IAlert | IProgressDialog) => {
+    const { key, id, caption, onCancel } = data;
+    return (
+      <Modal
+        className={styles.nonstop}
+        key={`${key}-${id}`}
+        style={{
+          minWidth: 150,
+        }}
+        width="fit-content"
+        open={alertProgressStack.length > 0}
+        onCancel={() => {
+          popById(id);
+          onCancel();
+        }}
+        centered
+        closable={false}
+        maskClosable={false}
+        cancelText={LANG.alert.cancel}
+        cancelButtonProps={{ style: { display: 'none' } }}
+        okButtonProps={{ style: { display: 'none' } }}
+      >
+        <div>
+          <div className={styles['spinner-container']}>
+            <SpinLoading color="primary" style={{ '--size': '48px' }} />
+          </div>
+          <div className={styles.caption}>{caption}</div>
+        </div>
+      </Modal>
+    );
+  };
+
+  const renderProgress = (data: IProgressDialog) => {
+    const { percentage, key, id, caption, onCancel } = data;
+    return (
+      <Modal
+        key={`${key}-${id}`}
+        style={{
+          minWidth: window.outerWidth < 600 ? window.outerWidth - 40 : 520,
+        }}
+        width={window.outerWidth < 600 ? window.outerWidth - 40 : 520}
+        open={alertProgressStack.length > 0}
+        title={caption}
+        onCancel={() => {
+          popById(id);
+          onCancel();
+        }}
+        centered
+        closable={false}
+        maskClosable={false}
+        cancelText={LANG.alert.cancel}
+        okButtonProps={{ style: { display: 'none' } }}
+      >
+        {renderMessage(data.message)}
+        <Progress
+          status="active"
+          percent={Number(Number(percentage).toFixed(2))}
+          strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+        />
+      </Modal>
+    );
+  };
+
   const alertModals = alertProgressStack.map((data) => {
     if (isProgress(data)) {
-      const { percentage } = data;
       if (data.type === ProgressConstants.NONSTOP) {
-        return (
-          <Modal
-            className={styles.nonstop}
-            key={`${data.key}-${data.id}`}
-            style={{
-              minWidth: 150,
-            }}
-            width="fit-content"
-            open={alertProgressStack.length > 0}
-            title={data.caption}
-            onCancel={() => {
-              popById(data.id);
-              data.onCancel();
-            }}
-            centered
-            closable={false}
-            maskClosable={false}
-            cancelText={LANG.alert.cancel}
-            okButtonProps={{ style: { display: 'none' } }}
-          >
-            <div>
-              <LoadingGridAnimation />
-            </div>
-          </Modal>
-        );
+        return renderNonstop(data);
       }
-      return (
-        <Modal
-          key={`${data.key}-${data.id}`}
-          style={{
-            minWidth: window.outerWidth < 600 ? (window.outerWidth - 40) : 520,
-          }}
-          width={window.outerWidth < 600 ? (window.outerWidth - 40) : 520}
-          open={alertProgressStack.length > 0}
-          title={data.caption}
-          onCancel={() => {
-            popById(data.id);
-            data.onCancel();
-          }}
-          centered
-          closable={false}
-          maskClosable={false}
-          cancelText={LANG.alert.cancel}
-          okButtonProps={{ style: { display: 'none' } }}
-        >
-          {renderMessage(data.message)}
-          <Progress
-            status="active"
-            percent={Number(Number(percentage).toFixed(2))}
-            strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
-          />
-        </Modal>
-      );
+      return renderProgress(data);
     }
 
     return <Alert key={`${data.key}-${data.id}`} data={data} />;
   });
 
-  return (
-    <div>
-      {alertModals}
-    </div>
-  );
+  return <div>{alertModals}</div>;
 };
 
 export default AlertsAndProgress;
