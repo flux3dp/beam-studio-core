fd83f7130da29d510670fb980f28319fd0e86ad6
Dean Sung <dean@flux3dp.com>
2023-09-07 18:34:13 +0800

    Extract svgStringToCanvas

---
+++ src/web/helpers/image/svgStringToCanvas.ts
@@ -0,0 +1,26 @@
+const svgStringToCanvas = (
+  svgString: string,
+  width: number,
+  height: number
+): Promise<HTMLCanvasElement> => {
+  const tempCanvas = document.createElement('canvas');
+  tempCanvas.width = width;
+  tempCanvas.height = height;
+  const ctx = tempCanvas.getContext('2d');
+  const svgUrl = `data:image/svg+xml; charset=utf8, ${encodeURIComponent(svgString)}`;
+  const img = new Image();
+  return new Promise((resolve, reject) => {
+    try {
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0, width, height);
+        URL.revokeObjectURL(svgUrl);
+        resolve(tempCanvas);
+      };
+    } catch (error) {
+      reject(error);
+    }
+    img.src = svgUrl;
+  });
+};
+
+export default svgStringToCanvas;
