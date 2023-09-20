99177d66db0d754a79eff5ba90f5acdc2ff30b88
Dean Sung <dean@flux3dp.com>
2023-09-20 14:17:33 +0800

    Fix SelLayerBlock display value

--- src/web/app/components/beambox/right-panel/SelLayerBlock.tsx
+++ src/web/app/components/beambox/right-panel/SelLayerBlock.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, memo } from 'react';
+import React, { memo, useContext, useEffect, useState } from 'react';
 
 import Alert from 'app/actions/alert-caller';
 import AlertConstants from 'app/constants/alert-constants';
@@ -16,14 +16,31 @@ const defaultOption = ' ';
 
 function SelLayerBlock(): JSX.Element {
   const lang = useI18n().beambox.right_panel.layer_panel;
-  const [promptMoveLayerOnce, setPromptMoveLayerOnce] = React.useState(false);
+  const [promptMoveLayerOnce, setPromptMoveLayerOnce] = useState(false);
+  const [displayValue, setDisplayValue] = useState(defaultOption);
   const { selectedElem } = useContext(CanvasContext);
+  useEffect(() => {
+    if (!selectedElem) return;
+    if (selectedElem.getAttribute('data-tempgroup') === 'true') {
+      const originalLayers = new Set([...selectedElem.childNodes].map((elem) =>
+        (elem as SVGElement).getAttribute('data-original-layer')
+      ));
+      if (originalLayers.size === 1) {
+        const [firstValue] = originalLayers;
+        setDisplayValue(firstValue ?? defaultOption);
+      }
+      else setDisplayValue(defaultOption);
+    } else {
+      const currentLayer = getObjectLayer(selectedElem as SVGElement);
+      const currentLayerName = currentLayer?.title ?? defaultOption;
+      setDisplayValue(currentLayerName);
+    }
+  }, [selectedElem]);
   if (!selectedElem) return null;
 
   const drawing = svgCanvas.getCurrentDrawing();
   const layerCount = drawing.getNumLayers();
   if (layerCount === 1) return null;
-
 
   const moveToOtherLayer = (e: React.ChangeEvent): void => {
     const select = e.target as HTMLSelectElement;
@@ -35,6 +52,7 @@ function SelLayerBlock(): JSX.Element {
       }
       svgCanvas.moveSelectedToLayer(destLayer);
       drawing.setCurrentLayer(destLayer);
+      setDisplayValue(destLayer);
       setPromptMoveLayerOnce(true);
     };
 
@@ -52,22 +70,27 @@ function SelLayerBlock(): JSX.Element {
     }
   };
 
-  const currentLayer = getObjectLayer(selectedElem as SVGElement);
-  const currentLayerName = currentLayer?.title ?? defaultOption;
-
   const options = [];
-  if (!currentLayer) {
-    options.push(<option value={defaultOption} key={-1}>{lang.move_elems_to}</option>);
+  if (displayValue === defaultOption) {
+    options.push(
+      <option value={defaultOption} key={-1}>
+        {lang.move_elems_to}
+      </option>
+    );
   }
   for (let i = layerCount - 1; i >= 0; i -= 1) {
     const layerName = drawing.getLayerName(i);
-    options.push(<option value={layerName} key={i}>{layerName}</option>);
+    options.push(
+      <option value={layerName} key={i}>
+        {layerName}
+      </option>
+    );
   }
   return (
     <div className="selLayerBlock controls">
       <span id="selLayerLabel">{lang.move_elems_to}</span>
       <select
-        value={currentLayerName}
+        value={displayValue}
         id="selLayerNames"
         title="Move selected elements to a different layer"
         onChange={(e: React.ChangeEvent) => moveToOtherLayer(e)}
