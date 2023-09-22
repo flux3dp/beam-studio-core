2fc8888d7521b93f358c29b194353334b058078c
Dean Sung <dean@flux3dp.com>
2023-09-22 18:10:34 +0800

    Fix splitFullColor bounding box calculation

--- src/web/helpers/layer/full-color/splitFullColorLayer.ts
+++ src/web/helpers/layer/full-color/splitFullColorLayer.ts
@@ -25,36 +25,52 @@ const layerToImage = async (
   layer: SVGGElement,
   dpi: number
 ): Promise<{ blob: Blob; bbox: { x: number; y: number; width: number; height: number } }> => {
-  const bbox = layer.getBBox();
-  let { x, y, width, height } = bbox;
-  if (x < 0) {
-    width += x;
-    x = 0;
-  }
-  if (y < 0) {
-    height += y;
-    y = 0;
-  }
-  if (x + width > svgCanvas.contentW) width = svgCanvas.contentW - x;
-  if (y + height > svgCanvas.contentH) height = svgCanvas.contentH - y;
   const layerClone = layer.cloneNode(true) as SVGGElement;
-  const targetWidth = Math.round((width * dpi) / (25.4 * constant.dpmm));
-  const targetHeight = Math.round((height * dpi) / (25.4 * constant.dpmm));
+  const canvasWidth = Math.round((svgCanvas.contentW * dpi) / (25.4 * constant.dpmm));
+  const canvasHeight = Math.round((svgCanvas.contentH * dpi) / (25.4 * constant.dpmm));
   const svgString = `
     <svg
-      width="${width}"
-      height="${height}"
-      viewBox="${x} ${y} ${width} ${height}"
+      width="${canvasWidth}"
+      height="${canvasHeight}"
+      viewBox="0 0 ${canvasWidth} ${canvasHeight}"
       xmlns:svg="http://www.w3.org/2000/svg"
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
     >
       ${layerClone.outerHTML}
     </svg>`;
-  const canvas = await svgStringToCanvas(svgString, targetWidth, targetHeight);
+  const canvas = await svgStringToCanvas(svgString, canvasWidth, canvasHeight);
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const { data } = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  const bounds = { minX: canvasWidth, minY: canvasHeight, maxX: 0, maxY: 0 };
+  for (let y = 0; y < canvasHeight; y += 1) {
+    for (let x = 0; x < canvasWidth; x += 1) {
+      const i = (y * canvasWidth + x) * 4;
+      const alpha = data[i + 3];
+      if (alpha > 0) {
+        if (x < bounds.minX) bounds.minX = x;
+        if (x > bounds.maxX) bounds.maxX = x;
+        if (y < bounds.minY) bounds.minY = y;
+        if (y > bounds.maxY) bounds.maxY = y;
+      }
+    }
+  }
+  if (bounds.minX > bounds.maxX || bounds.minY > bounds.maxY)
+    return { blob: null, bbox: { x: 0, y: 0, width: 0, height: 0 } };
+  const bbox = {
+    x: bounds.minX,
+    y: bounds.minY,
+    width: bounds.maxX - bounds.minX + 1,
+    height: bounds.maxY - bounds.minY + 1,
+  };
+  const newImageData = ctx.getImageData(bbox.x, bbox.y, bbox.width, bbox.height);
+  canvas.width = bbox.width;
+  canvas.height = bbox.height;
+  ctx.putImageData(newImageData, 0, 0);
+
   return new Promise((resolve) => {
     canvas.toBlob((b) => {
-      resolve({ blob: b, bbox: { x, y, width, height } });
+      resolve({ blob: b, bbox });
     });
   });
 };
