54c539499e475f69b31230c9b12ac769271292f3
Dean Sung <dean@flux3dp.com>
2023-10-17 18:39:50 +0800

    Update calibration lang

--- src/web/app/views/beambox/CameraCalibration/StepAskReadjust.tsx
+++ src/web/app/views/beambox/CameraCalibration/StepAskReadjust.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
-import * as React from 'react';
+import React, { useContext } from 'react';
 import { Button, Modal } from 'antd';
-import i18n from 'helpers/i18n';
+
 import PreviewModeController from 'app/actions/beambox/preview-mode-controller';
 import Progress from 'app/actions/progress-caller';
 import Alert from 'app/actions/alert-caller';
@@ -10,15 +10,20 @@ import DeviceErrorHandler from 'helpers/device-error-handler';
 import DeviceMaster from 'helpers/device-master';
 import Browser from 'implementations/browser';
 import CheckDeviceStatus from 'helpers/check-device-status';
+import useI18n from 'helpers/useI18n';
+import {
+  CALIBRATION_PARAMS,
+  STEP_BEFORE_ANALYZE_PICTURE,
+  STEP_PUT_PAPER,
+} from 'app/constants/camera-calibration-constants';
 import { doGetOffsetFromPicture } from 'helpers/camera-calibration-helper';
-import { CALIBRATION_PARAMS, STEP_BEFORE_ANALYZE_PICTURE, STEP_PUT_PAPER } from 'app/constants/camera-calibration-constants';
 import { CalibrationContext } from 'app/contexts/CalibrationContext';
-import { useContext } from 'react';
 
-const LANG = i18n.lang.camera_calibration;
-const LANG_ALERT = i18n.lang.alert;
 
 const StepAskReadjust = (): JSX.Element => {
+  const lang = useI18n();
+  const langCalibration = lang.calibration;
+  const langAlert = lang.alert;
   const context = useContext(CalibrationContext);
   const {
     device,
@@ -32,11 +37,13 @@ const StepAskReadjust = (): JSX.Element => {
 
   const onSkip = async () => {
     try {
-      await PreviewModeController.start(device, () => console.log('camera fail. stop preview mode'));
+      await PreviewModeController.start(device, () =>
+        console.log('camera fail. stop preview mode')
+      );
       setLastConfig(PreviewModeController.getCameraOffsetStandard());
       Progress.openNonstopProgress({
         id: 'taking-picture',
-        message: LANG.taking_picture,
+        message: langCalibration.taking_picture,
         timeout: 30000,
       });
       const x = CALIBRATION_PARAMS.centerX - CALIBRATION_PARAMS.idealOffsetX;
@@ -48,22 +55,26 @@ const StepAskReadjust = (): JSX.Element => {
       gotoNextStep(STEP_BEFORE_ANALYZE_PICTURE);
     } catch (error) {
       console.log(error);
-      const errorMessage = error instanceof Error
-        ? error.message : DeviceErrorHandler.translate(error);
+      const errorMessage =
+        error instanceof Error ? error.message : DeviceErrorHandler.translate(error);
       Alert.popUp({
         id: 'camera-cali-err',
         type: AlertConstants.SHOW_POPUP_ERROR,
         message: `#815 ${errorMessage || 'Fail to capture'}`,
-        buttonLabels: [LANG_ALERT.ok, LANG_ALERT.learn_more],
+        buttonLabels: [langAlert.ok, langAlert.learn_more],
         callbacks: [
           async () => {
             const report = await DeviceMaster.getReport();
-            await CheckDeviceStatus({
-              ...device,
-              st_id: report.st_id,
-            }, false, true);
+            await CheckDeviceStatus(
+              {
+                ...device,
+                st_id: report.st_id,
+              },
+              false,
+              true
+            );
           },
-          () => Browser.open(LANG.zendesk_link),
+          () => Browser.open(langCalibration.zendesk_link),
         ],
         primaryButtonIndex: 0,
       });
@@ -76,22 +87,18 @@ const StepAskReadjust = (): JSX.Element => {
       width={400}
       open
       centered
-      title={LANG.camera_calibration}
+      title={langCalibration.camera_calibration}
       onCancel={() => onClose(false)}
       className="modal-camera-calibration"
       footer={[
-        <Button onClick={() => onClose(false)}>
-          {LANG.cancel}
-        </Button>,
-        <Button onClick={onSkip}>
-          {LANG.skip}
-        </Button>,
+        <Button onClick={() => onClose(false)}>{langCalibration.cancel}</Button>,
+        <Button onClick={onSkip}>{langCalibration.skip}</Button>,
         <Button type="primary" onClick={() => gotoNextStep(STEP_PUT_PAPER)}>
-          {LANG.do_engraving}
+          {langCalibration.do_engraving}
         </Button>,
       ]}
     >
-      {LANG.ask_for_readjust}
+      {langCalibration.ask_for_readjust}
     </Modal>
   );
 };
