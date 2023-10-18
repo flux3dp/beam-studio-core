d5a024c24322be983981df00fdbafd7783addccf
Dean Sung <dean@flux3dp.com>
2023-10-18 12:39:43 +0800

    Remove debug console.log

--- src/web/app/pages/Beambox.tsx
+++ src/web/app/pages/Beambox.tsx
@@ -39,7 +39,6 @@ const Beambox = (): JSX.Element => {
   });
 
   const activeLang = i18n.getActiveLang();
-  console.log('beambox rerender');
   return (
     <CanvasProvider>
       <SelectedElementContextProvider>
