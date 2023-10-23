61341e5ef1018fe080a4e3880c1d5d0c83d01c88
EstherFu <sloborfu@gmail.com>
2023-10-23 18:37:18 +0800

    add mobile module select and ink select

--- src/web/app/views/beambox/Right-Panels/ConfigPanel/UVBlock.tsx
+++ src/web/app/views/beambox/Right-Panels/ConfigPanel/UVBlock.tsx
@@ -1,13 +1,16 @@
 import classNames from 'classnames';
 import React, { memo, useContext } from 'react';
 
-import { DataType, writeData } from 'helpers/layer/layer-config-helper';
+import ObjectPanelItem from 'app/views/beambox/Right-Panels/ObjectPanelItem';
 // import useI18n from 'helpers/useI18n';
+import { DataType, writeData } from 'helpers/layer/layer-config-helper';
+import { useIsMobile } from 'helpers/system-helper';
 
 import ConfigPanelContext from './ConfigPanelContext';
 import styles from './Block.module.scss';
 
 const UVBlock = (): JSX.Element => {
+  const isMobile = useIsMobile();
   // const lang = useI18n();
   // const t = lang.beambox.right_panel.laser_panel;
   const { selectedLayers, state, dispatch } = useContext(ConfigPanelContext);
@@ -19,7 +22,18 @@ const UVBlock = (): JSX.Element => {
     selectedLayers.forEach((layerName) => writeData(layerName, DataType.UV, newValue));
   };
 
-  return (
+  return isMobile ? (
+    <ObjectPanelItem.Select
+      id="ink-type"
+      selected={uv.value === 1 ? { value: 1, label: 'UV墨' } : { value: 0, label: '一般墨' }}
+      onChange={handleToggle}
+      options={[
+        { value: 0, label: '一般墨' },
+        { value: 1, label: 'UV墨' },
+      ]}
+      label="Ink Type"
+    />
+  ) : (
     <div className={classNames(styles.panel, styles.checkbox)} onClick={handleToggle}>
       <span className={styles.title}>UV</span>
       <input type="checkbox" checked={uv.value === 1} readOnly />
