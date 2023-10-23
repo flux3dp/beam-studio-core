c77119c067d2e75639934812a2ab41845cb3b17d
EstherFu <sloborfu@gmail.com>
2023-10-23 18:00:51 +0800

    Add icons

--- src/web/app/icons/layer-panel/LayerPanelIcons.tsx
+++ src/web/app/icons/layer-panel/LayerPanelIcons.tsx
@@ -1,21 +1,29 @@
 import Add from './add.svg';
+import Expand from './expand.svg';
 import FullColor from './fullcolor.svg';
+import Invisible from './invisible.svg';
+import Laser from './laser.svg';
 import Lock from './lock.svg';
 import Merge from './merge.svg';
 import Move from './move.svg';
-import Laser from './laser.svg';
 import Print from './print.svg';
 import Rename from './rename.svg';
+import Setting from './setting.svg';
 import Unlock from './unlock.svg';
+import Visible from './visible.svg';
 
 export default {
   Add,
+  Expand,
   FullColor,
+  Invisible,
+  Laser,
   Lock,
   Merge,
   Move,
-  Laser,
   Print,
   Rename,
+  Setting,
   Unlock,
+  Visible,
 };
