8f038b2a79973322e74ab2325739bff41f06fc7e
Dean Sung <dean@flux3dp.com>
2023-11-03 14:02:27 +0800

    Update 2w default module boundary

--- src/web/app/constants/layer-module/module-boundary.ts
+++ src/web/app/constants/layer-module/module-boundary.ts
@@ -4,7 +4,7 @@ import LayerModule from 'app/constants/layer-module/layer-modules';
 const moduleBoundary = {
   [LayerModule.LASER_10W_DIODE]: { top: 0, left: 0, bottom: 0, right: 0 },
   [LayerModule.LASER_20W_DIODE]: { top: 0, left: 0, bottom: 10, right: 0 },
-  [LayerModule.LASER_1064]: { top: 26.95, left: 0, bottom: 18, right: 0 },
+  [LayerModule.LASER_1064]: { top: 0, left: 0, bottom: 18, right: 0 },
   [LayerModule.PRINTER]: { top: 12.7, left: 0, bottom: 30, right: 0 },
 };
 
