edad05e363a96373998afb3b0552a295d6077f93
Dean Sung <dean@flux3dp.com>
2024-01-04 11:11:00 +0800

    Fix openBottomBoundary update called twice

--- src/web/app/actions/beambox/open-bottom-boundary-drawer.ts
+++ src/web/app/actions/beambox/open-bottom-boundary-drawer.ts
@@ -1,80 +1,90 @@
-import Constant from './constant';
+import NS from 'app/constants/namespaces';
+
 import BeamboxPreference from './beambox-preference';
+import Constant from './constant';
 import { getSVGAsync } from '../../../helpers/svg-editor-helper';
 
 let svgedit;
 getSVGAsync((globalSVG) => {
-    svgedit = globalSVG.Edit;
+  svgedit = globalSVG.Edit;
 });
 
-let openBottomBoundaryRect, openBottomBoundarySVG;
+let openBottomBoundaryRect: SVGRectElement;
+let openBottomBoundarySVG: SVGSVGElement;
 
-const checkSvgEdit = () => {
-    return new Promise((resolve) => {
-        if (svgedit) {
-            resolve(null);
-            return;
-        }
-        const interval = setInterval(() => {
-            if (svgedit) {
-                resolve(null);
-                clearInterval(interval);
-            }
-        }, 500);
-    });
-}
+const checkSvgEdit = () =>
+  new Promise((resolve) => {
+    if (svgedit) {
+      resolve(null);
+      return;
+    }
+    const interval = setInterval(() => {
+      if (svgedit) {
+        resolve(null);
+        clearInterval(interval);
+      }
+    }, 500);
+  });
 
 const createBoundary = async () => {
-    await checkSvgEdit();
-    openBottomBoundarySVG = document.createElementNS(svgedit.NS.SVG, 'svg');
-    openBottomBoundaryRect = document.createElementNS(svgedit.NS.SVG, 'rect');
-    const canvasBackground = svgedit.utilities.getElem('canvasBackground');
-    canvasBackground.appendChild(openBottomBoundarySVG);
-    openBottomBoundarySVG.appendChild(openBottomBoundaryRect);
-    svgedit.utilities.assignAttributes(openBottomBoundarySVG, {
-        'id': 'open-bottom-boundary',
-        'width': '100%',
-        'height': '100%',
-        'viewBox': `0 0 ${Constant.dimension.getWidth(BeamboxPreference.read('workarea'))} ${Constant.dimension.getHeight(BeamboxPreference.read('workarea'))}`,
-        'x': 0,
-        'y': 0,
-        'style': 'pointer-events:none',
-    });
-    svgedit.utilities.assignAttributes(openBottomBoundaryRect, {
-        'fill': '#CCC',
-        'fill-opacity': 0.4,
-        'fill-rule': 'evenodd',
-        'stroke': 'none',
-        'style': 'pointer-events:none',
-        'y': 0,
-        'width': Constant.borderless.safeDistance.X * Constant.dpmm,
-        'height': '100%',
-    });
-};
-
-const update = () => {
-    const isOpenBottom = BeamboxPreference.read('borderless');
-    const supportOpenBottom = Constant.addonsSupportList.openBottom.includes(BeamboxPreference.read('workarea'));
-    if (isOpenBottom && supportOpenBottom) {
-        show();
-    } else {
-        hide();
-    }
+  await checkSvgEdit();
+  openBottomBoundarySVG = document.createElementNS(
+    NS.SVG,
+    'svg'
+  ) as unknown as SVGSVGElement;
+  openBottomBoundaryRect = document.createElementNS(
+    NS.SVG,
+    'rect'
+  ) as unknown as SVGRectElement;
+  const canvasBackground = svgedit.utilities.getElem('canvasBackground');
+  canvasBackground.appendChild(openBottomBoundarySVG);
+  openBottomBoundarySVG.appendChild(openBottomBoundaryRect);
+  openBottomBoundarySVG.id = 'open-bottom-boundary';
+  openBottomBoundarySVG.setAttribute('width', '100%');
+  openBottomBoundarySVG.setAttribute('height', '100%');
+  const workarea = BeamboxPreference.read('workarea');
+  openBottomBoundarySVG.setAttribute(
+    'viewBox',
+    `0 0 ${Constant.dimension.getWidth(workarea)} ${Constant.dimension.getHeight(workarea)}`
+  );
+  openBottomBoundarySVG.setAttribute('x', '0');
+  openBottomBoundarySVG.setAttribute('y', '0');
+  openBottomBoundarySVG.setAttribute('style', 'pointer-events:none');
+  openBottomBoundaryRect.setAttribute('fill', '#CCC');
+  openBottomBoundaryRect.setAttribute('fill-opacity', '0.4');
+  openBottomBoundaryRect.setAttribute('fill-rule', 'evenodd');
+  openBottomBoundaryRect.setAttribute('stroke', 'none');
+  openBottomBoundaryRect.setAttribute('style', 'pointer-events:none');
+  openBottomBoundaryRect.setAttribute('y', '0');
+  openBottomBoundaryRect.setAttribute(
+    'width',
+    `${Constant.borderless.safeDistance.X * Constant.dpmm}`
+  );
+  openBottomBoundaryRect.setAttribute('height', '100%');
 };
 
 const show = async () => {
-    if (!document.getElementById('open-bottom-boundary')) {
-        await createBoundary();
-    }
-    const x = Constant.dimension.getWidth(BeamboxPreference.read('workarea')) - Constant.borderless.safeDistance.X * Constant.dpmm;
-    openBottomBoundaryRect.setAttribute('x', x);
-    openBottomBoundaryRect.setAttribute('display', 'block');
+  if (!openBottomBoundarySVG) await createBoundary();
+  const x =
+    Constant.dimension.getWidth(BeamboxPreference.read('workarea')) -
+    Constant.borderless.safeDistance.X * Constant.dpmm;
+  openBottomBoundaryRect.setAttribute('x', x.toString());
+  openBottomBoundaryRect.setAttribute('display', 'block');
 };
 const hide = () => {
-    if (!openBottomBoundaryRect) return;
-    openBottomBoundaryRect.setAttribute('display', 'none');
-}
+  if (!openBottomBoundaryRect) return;
+  openBottomBoundaryRect.setAttribute('display', 'none');
+};
+
+const update = (): void => {
+  const isOpenBottom = BeamboxPreference.read('borderless');
+  const supportOpenBottom = Constant.addonsSupportList.openBottom.includes(
+    BeamboxPreference.read('workarea')
+  );
+  if (isOpenBottom && supportOpenBottom) show();
+  else hide();
+};
 
 export default {
-    update,
+  update,
 };
