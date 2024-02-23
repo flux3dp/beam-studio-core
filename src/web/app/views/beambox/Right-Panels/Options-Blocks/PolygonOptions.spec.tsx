69efb0953b5d1182399b472e71aae84662ea5bfc
Dean Sung <dean@flux3dp.com>
2024-02-23 19:24:13 +0800

    Add undo for lock and polygon sides

--- src/web/app/views/beambox/Right-Panels/Options-Blocks/PolygonOptions.spec.tsx
+++ src/web/app/views/beambox/Right-Panels/Options-Blocks/PolygonOptions.spec.tsx
@@ -5,47 +5,66 @@ import { ObjectPanelContextProvider } from 'app/views/beambox/Right-Panels/conte
 
 import PolygonOptions from './PolygonOptions';
 
-jest.mock('app/views/beambox/Right-Panels/Options-Blocks/InFillBlock', () => () => <div>DummyInFillBlock</div>);
-
 const useIsMobile = jest.fn();
 jest.mock('helpers/system-helper', () => ({
   useIsMobile: () => useIsMobile(),
 }));
 
-jest.mock('helpers/i18n', () => ({
-  lang: {
-    beambox: {
-      right_panel: {
-        object_panel: {
-          option_panel: {
-            sides: 'Sides',
-          },
+jest.mock('helpers/useI18n', () => () => ({
+  beambox: {
+    right_panel: {
+      object_panel: {
+        option_panel: {
+          sides: 'Sides',
         },
       },
     },
   },
 }));
 
-window.polygonAddSides = jest.fn(() => {
-  const sides = +document.getElementById('flux').getAttribute('sides');
-  document.getElementById('flux').setAttribute('sides', (sides + 1).toString());
+const mockUpdatePolygonSides = jest.fn((elem, val) => {
+  const sides = +elem.getAttribute('sides');
+  elem.setAttribute('sides', (sides + val).toString());
 });
-window.polygonDecreaseSides = jest.fn(() => {
-  const sides = +document.getElementById('flux').getAttribute('sides');
-  document.getElementById('flux').setAttribute('sides', Math.max((sides - 1),3).toString());
-});
+
+const mockCreateBatchCommand = jest.fn();
+const mockBatchCmd = {
+  addSubCommand: jest.fn(),
+  isEmpty: jest.fn(),
+};
+jest.mock('app/svgedit/HistoryCommandFactory', () => ({
+  createBatchCommand: (...args) => mockCreateBatchCommand(...args),
+}));
+
+const mockBeginUndoableChange = jest.fn();
+const mockFinishUndoableChange = jest.fn();
+const mockAddCommandToHistory = jest.fn();
+jest.mock('helpers/svg-editor-helper', () => ({
+  getSVGAsync: (cb) => {
+    cb({
+      Canvas: {
+        undoMgr: {
+          beginUndoableChange: (...args) => mockBeginUndoableChange(...args),
+          finishUndoableChange: (...args) => mockFinishUndoableChange(...args),
+          addCommandToHistory: (...args) => mockAddCommandToHistory(...args),
+        },
+      },
+    });
+  },
+}));
+
 
 describe('test PolygonOptions', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    window.updatePolygonSides = mockUpdatePolygonSides;
+    mockCreateBatchCommand.mockReturnValue(mockBatchCmd);
+    mockFinishUndoableChange.mockReturnValue(mockBatchCmd);
   });
 
   test('should render correctly', () => {
     const { container, rerender } = render(
-      <PolygonOptions
-        elem={document.getElementById('flux')}
-        polygonSides={0}
-      />
+      <PolygonOptions elem={document.getElementById('flux')} polygonSides={0} />
     );
     expect(container).toMatchSnapshot();
 
@@ -53,12 +72,7 @@ describe('test PolygonOptions', () => {
     elem.setAttribute('id', 'flux');
     elem.setAttribute('sides', '5');
     document.body.appendChild(elem);
-    rerender(
-      <PolygonOptions
-        elem={document.getElementById('flux')}
-        polygonSides={5}
-      />
-    );
+    rerender(<PolygonOptions elem={document.getElementById('flux')} polygonSides={5} />);
 
     expect(container).toMatchSnapshot();
 
@@ -67,32 +81,43 @@ describe('test PolygonOptions', () => {
     fireEvent.blur(input);
 
     expect(container).toMatchSnapshot();
-    expect(window.polygonAddSides).toHaveBeenCalledTimes(3);
-    expect(window.polygonDecreaseSides).not.toHaveBeenCalled();
+    expect(mockCreateBatchCommand).toBeCalledTimes(1);
+    expect(mockCreateBatchCommand).toHaveBeenNthCalledWith(1, 'Change Polygon Sides');
+    expect(mockUpdatePolygonSides).toHaveBeenCalledTimes(1);
+    expect(mockUpdatePolygonSides).toHaveBeenCalledWith(elem, 3);
+    expect(mockBeginUndoableChange).toBeCalledTimes(2);
+    expect(mockBeginUndoableChange).toHaveBeenNthCalledWith(1, 'sides', [elem]);
+    expect(mockBeginUndoableChange).toHaveBeenNthCalledWith(2, 'points', [elem]);
+    expect(mockFinishUndoableChange).toBeCalledTimes(2);
+    expect(mockBatchCmd.isEmpty).toBeCalledTimes(3);
+    expect(mockBatchCmd.addSubCommand).toBeCalledTimes(2);
+    expect(mockAddCommandToHistory).toBeCalledTimes(1);
+    expect(mockAddCommandToHistory).toHaveBeenNthCalledWith(1, mockBatchCmd);
 
     jest.clearAllMocks();
 
     fireEvent.change(input, { target: { value: 5 } });
     fireEvent.blur(input);
     expect(container).toMatchSnapshot();
-    expect(window.polygonAddSides).not.toHaveBeenCalled();
-    expect(window.polygonDecreaseSides).toHaveBeenCalledTimes(3);
+    expect(mockUpdatePolygonSides).toHaveBeenCalledTimes(1);
+    expect(mockUpdatePolygonSides).toHaveBeenCalledWith(elem, -3);
 
     jest.clearAllMocks();
 
     fireEvent.change(input, { target: { value: 5 } });
     fireEvent.blur(input);
-    expect(window.polygonAddSides).not.toHaveBeenCalled();
-    expect(window.polygonDecreaseSides).not.toHaveBeenCalled();
+    expect(mockUpdatePolygonSides).not.toHaveBeenCalled();
+    expect(mockBeginUndoableChange).not.toBeCalled();
+    expect(mockFinishUndoableChange).not.toBeCalled();
+    expect(mockBatchCmd.isEmpty).not.toBeCalled();
+    expect(mockBatchCmd.addSubCommand).not.toBeCalled();
+    expect(mockAddCommandToHistory).not.toBeCalled();
   });
 
-  test('should render correctly in mobile', () => {
-    useIsMobile.mockReturnValue(true)
+  test('should render correctly in mobile', async () => {
+    useIsMobile.mockReturnValue(true);
     const { baseElement, container, getByText, rerender } = render(
-      <PolygonOptions
-        elem={document.getElementById('flux')}
-        polygonSides={0}
-      />
+      <PolygonOptions elem={document.getElementById('flux')} polygonSides={0} />
     );
     expect(container).toMatchSnapshot();
 
@@ -102,10 +127,7 @@ describe('test PolygonOptions', () => {
     document.body.appendChild(elem);
     rerender(
       <ObjectPanelContextProvider>
-        <PolygonOptions
-          elem={document.getElementById('flux')}
-          polygonSides={5}
-        />
+        <PolygonOptions elem={document.getElementById('flux')} polygonSides={5} />
       </ObjectPanelContextProvider>
     );
     expect(container).toMatchSnapshot();
@@ -117,12 +139,11 @@ describe('test PolygonOptions', () => {
     fireEvent.click(objectPanelItem);
     expect(objectPanelItem).toHaveClass('active');
     expect(getByText('.').parentElement).toHaveClass('adm-button-disabled');
-    expect(window.polygonAddSides).not.toHaveBeenCalled();
-    expect(window.polygonDecreaseSides).not.toHaveBeenCalled();
+    expect(mockUpdatePolygonSides).not.toHaveBeenCalled();
+    fireEvent.click(baseElement.querySelectorAll('.input-keys button')[2]);
 
-    fireEvent.click(baseElement.querySelectorAll('.input-keys button')[11]);
-    expect(window.polygonAddSides).not.toHaveBeenCalled();
-    expect(window.polygonDecreaseSides).toHaveBeenCalledTimes(5);
+    expect(mockUpdatePolygonSides).toHaveBeenCalledTimes(1);
+    expect(mockUpdatePolygonSides).toHaveBeenLastCalledWith(expect.anything(), -2);
     expect(expect(displayBtn).toHaveTextContent('3'));
   });
-})
+});
