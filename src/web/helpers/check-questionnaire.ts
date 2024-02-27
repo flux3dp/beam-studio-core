51747692ddeee16bc22b2256b6d9288c4c784731
Dean Sung <dean@flux3dp.com>
2024-02-27 11:39:03 +0800

    Update questionnaire

--- src/web/helpers/check-questionnaire.ts
+++ src/web/helpers/check-questionnaire.ts
@@ -1,28 +1,36 @@
 import storage from 'implementations/storage';
 
-const MIN_ALLOWED_VERSION = 3;
+const MIN_ALLOWED_VERSION = 2;
 
 let resultCache = null;
 
-const checkQuestionnaire = async () => {
-  if (resultCache) return resultCache;
+const checkQuestionnaire = async (
+  opts: {
+    allowOldVersion?: boolean;
+    useCache?: boolean;
+  } = {}
+): Promise<{ version: number; urls: { [key: string]: string } }> => {
+  const { allowOldVersion = false, useCache = true } = opts;
 
-  return fetch('https://id.flux3dp.com/api/questionnaire/1')
-    .then((response) => {
-      if (response.status !== 200) {
-        throw new Error();
-      }
-      return response.json();
-    })
-    .then((myJson) => {
-      const lastQuestionnaireVersion = storage.get('questionnaire-version') || 0;
-      if (myJson.version > lastQuestionnaireVersion && myJson.version >= MIN_ALLOWED_VERSION) {
-        resultCache = myJson;
-        return myJson;
-      }
-      return null;
-    })
-    .catch(() => null);
+  try {
+    let result;
+    if (resultCache && useCache) {
+      result = resultCache;
+    } else {
+      const resp = await fetch('https://id.flux3dp.com/api/questionnaire/1');
+      result = await resp.json();
+      resultCache = result;
+    }
+    const lastQuestionnaireVersion = storage.get('questionnaire-version') || 0;
+    console.log(lastQuestionnaireVersion, result);
+    if (!allowOldVersion && lastQuestionnaireVersion >= result.version) return null;
+    if (result.version >= MIN_ALLOWED_VERSION) {
+      return result;
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
 };
 
 export default checkQuestionnaire;
