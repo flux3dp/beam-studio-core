4ed50d233a777086eb299596a35e91589aa1d31f
Dean Sung <dean@flux3dp.com>
2024-04-12 18:30:54 +0800

    Move ai-extension out from describe

--- src/web/helpers/api/ai-extension.spec.ts
+++ src/web/helpers/api/ai-extension.spec.ts
@@ -16,26 +16,24 @@ jest.mock('helpers/layer/layer-config-helper', () => ({
 // eslint-disable-next-line import/first
 import aiExtension from './ai-extension';
 
-describe('', () => {
-  test('ai-extension', async () => {
-    aiExtension.init();
-    expect(mockWebsocket).toBeCalledWith({
-      method: 'push-studio',
-      onMessage: expect.any(Function),
-      onError: expect.any(Function),
-      onFatal: expect.any(Function),
-    });
-    const { onMessage } = mockWebsocket.mock.calls[0][0];
-    await onMessage({
-      svg: 'svg',
-      layerData: '{"layer1": {"name": "layer1", "speed": "10", "power": "20"}}',
-    });
-    expect(mockImportSvg).toBeCalledTimes(1);
-    expect(mockImportSvg).toBeCalledWith(new Blob(['svg'], { type: 'text/plain' }), {
-      isFromAI: true,
-    });
-    expect(mockWriteData).toBeCalledTimes(2);
-    expect(mockWriteData).toHaveBeenNthCalledWith(1, 'layer1', 'speed', 10);
-    expect(mockWriteData).toHaveBeenNthCalledWith(2, 'layer1', 'strength', 20);
+test('ai-extension', async () => {
+  aiExtension.init();
+  expect(mockWebsocket).toBeCalledWith({
+    method: 'push-studio',
+    onMessage: expect.any(Function),
+    onError: expect.any(Function),
+    onFatal: expect.any(Function),
   });
+  const { onMessage } = mockWebsocket.mock.calls[0][0];
+  await onMessage({
+    svg: 'svg',
+    layerData: '{"layer1": {"name": "layer1", "speed": "10", "power": "20"}}',
+  });
+  expect(mockImportSvg).toBeCalledTimes(1);
+  expect(mockImportSvg).toBeCalledWith(new Blob(['svg'], { type: 'text/plain' }), {
+    isFromAI: true,
+  });
+  expect(mockWriteData).toBeCalledTimes(2);
+  expect(mockWriteData).toHaveBeenNthCalledWith(1, 'layer1', 'speed', 10);
+  expect(mockWriteData).toHaveBeenNthCalledWith(2, 'layer1', 'strength', 20);
 });
