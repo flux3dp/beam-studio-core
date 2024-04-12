35c32e15f688a99631f22d82c9390e68c6a5859f
Dean Sung <dean@flux3dp.com>
2024-04-12 10:36:42 +0800

    Update ColorRatioBlock

--- src/web/app/views/beambox/Right-Panels/ConfigPanel/ColorRatioBlock.spec.tsx
+++ src/web/app/views/beambox/Right-Panels/ConfigPanel/ColorRatioBlock.spec.tsx
@@ -3,26 +3,62 @@ import { fireEvent, render } from '@testing-library/react';
 
 import ColorRatioBlock from './ColorRatioBlock';
 
-const mockSetValue = jest.fn();
+const mockSetRatio = jest.fn();
+const mockSetSmooth = jest.fn();
 
 describe('test ColorRatioBlock', () => {
   it('should render correctly', () => {
     const { container } = render(
-      <ColorRatioBlock value={50} setValue={mockSetValue} color="c" />
+      <ColorRatioBlock
+        ratio={50}
+        setRatio={mockSetRatio}
+        smooth={1}
+        setSmooth={mockSetSmooth}
+        color="c"
+      />
     );
     expect(container).toMatchSnapshot();
   });
 
   test('setValue should work', () => {
     const { container, rerender } = render(
-      <ColorRatioBlock value={50} setValue={mockSetValue} color="c" />
+      <ColorRatioBlock
+        ratio={50}
+        setRatio={mockSetRatio}
+        smooth={1}
+        setSmooth={mockSetSmooth}
+        color="c"
+      />
     );
-    expect(mockSetValue).not.toBeCalled();
-    const input = container.querySelector('input');
-    fireEvent.change(input, { target: { value: 49 } });
-    expect(mockSetValue).toBeCalledTimes(1);
-    expect(mockSetValue).toHaveBeenLastCalledWith(49);
-    rerender(<ColorRatioBlock value={49} setValue={mockSetValue} color="c" />);
-    expect(input.value).toBe('49');
+    expect(mockSetRatio).not.toBeCalled();
+    const ratioInput = container.querySelectorAll('input')[0];
+    const smoothInput = container.querySelectorAll('input')[1];
+    fireEvent.change(ratioInput, { target: { value: 49 } });
+    expect(mockSetRatio).toBeCalledTimes(1);
+    expect(mockSetRatio).toHaveBeenLastCalledWith(49);
+    rerender(
+      <ColorRatioBlock
+        ratio={49}
+        setRatio={mockSetRatio}
+        smooth={1}
+        setSmooth={mockSetSmooth}
+        color="c"
+      />
+    );
+    expect(ratioInput.value).toBe('49');
+    fireEvent.change(smoothInput, { target: { value: 1.1 } });
+    expect(mockSetSmooth).toBeCalledTimes(1);
+    expect(mockSetSmooth).toHaveBeenLastCalledWith(1.1);
+    rerender(
+      <ColorRatioBlock
+        ratio={49}
+        setRatio={mockSetRatio}
+        smooth={1.1}
+        setSmooth={mockSetSmooth}
+        color="c"
+      />
+    );
+    expect(smoothInput.value).toBe('1.1');
+
   });
 });
