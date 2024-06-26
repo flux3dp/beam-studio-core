65d89230fbfa2a3d516d88216e699229bd5ad4c0
Dean Sung <dean@flux3dp.com>
2024-06-26 16:34:44 +0800

    Update Layer Display

--- src/web/app/views/beambox/Right-Panels/LayerPanel/LayerList.tsx
+++ src/web/app/views/beambox/Right-Panels/LayerPanel/LayerList.tsx
@@ -95,6 +95,13 @@ const LayerList = ({
       const isSelected = selectedLayers.includes(layerName);
       const isVis = drawing.getLayerVisibility(layerName);
       const module = getData<LayerModule>(layer, DataType.module);
+      const isRef = getData<boolean>(layer, DataType.ref);
+      console.log(layerName, isRef);
+      let moduleIcon = null;
+      if (isRef) moduleIcon = <LayerPanelIcons.Ref />;
+      else if (shouldShowModuleIcon)
+        moduleIcon =
+          module === LayerModule.PRINTER ? <LayerPanelIcons.Print /> : <LayerPanelIcons.Laser />;
       const layerItem = (
         <div
           key={layerName}
@@ -141,13 +148,9 @@ const LayerList = ({
                 />
               )}
             </div>
-            {shouldShowModuleIcon && (
+            {moduleIcon && (
               <div className={styles.module}>
-                {module === LayerModule.PRINTER ? (
-                  <LayerPanelIcons.Print />
-                ) : (
-                  <LayerPanelIcons.Laser />
-                )}
+                {moduleIcon}
               </div>
             )}
             <div
