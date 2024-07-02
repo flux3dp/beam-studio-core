491db966a0586cc94a56d6a09a81c07b959d3de9
Dean Sung <dean@flux3dp.com>
2024-07-02 15:58:24 +0800

    Fix test and add error handling

--- src/web/helpers/locale-helper.spec.ts
+++ src/web/helpers/locale-helper.spec.ts
@@ -1,62 +1,71 @@
-import localeHelper from './locale-helper';
-
 const mockParse = jest.fn();
 jest.mock('bcp-47', () => ({
   parse: (...args) => mockParse(...args),
 }));
+jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
+
+// eslint-disable-next-line import/first
+import localeHelper from './locale-helper';
+
+const mockConsoleError = jest.fn();
 
 describe('test locale-helper', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    console.error = mockConsoleError;
+  });
+
   test('when locale is en-US and timezone offset is 300', () => {
     const navigator = { language: 'en-US' };
-    const date = { getTimezoneOffset: () => 300 };
     Object.defineProperty(global, 'navigator', { value: navigator });
-    Object.defineProperty(global, 'Date', { value: date });
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
     mockParse.mockReturnValue({ region: 'US' });
-    expect(localeHelper.isNorthAmerica).toBe(true);
+    expect(localeHelper.detectNorthAmerica()).toBe(true);
+    expect(mockConsoleError).not.toBeCalled();
   });
 
   test('when locale is en-US and timezone offset is 660', () => {
     const navigator = { language: 'en-US' };
-    const date = { getTimezoneOffset: () => 660 };
     Object.defineProperty(global, 'navigator', { value: navigator });
-    Object.defineProperty(global, 'Date', { value: date });
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(660);
     mockParse.mockReturnValue({ region: 'US' });
-    expect(localeHelper.isNorthAmerica).toBe(false);
+    expect(localeHelper.detectNorthAmerica()).toBe(false);
+    expect(mockConsoleError).not.toBeCalled();
   });
 
   test('when locale is en-CA and timezone offset is 300', () => {
     const navigator = { language: 'en-CA' };
-    const date = { getTimezoneOffset: () => 300 };
     Object.defineProperty(global, 'navigator', { value: navigator });
-    Object.defineProperty(global, 'Date', { value: date });
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
     mockParse.mockReturnValue({ region: 'CA' });
-    expect(localeHelper.isNorthAmerica).toBe(true);
+    expect(localeHelper.detectNorthAmerica()).toBe(true);
+    expect(mockConsoleError).not.toBeCalled();
   });
 
   test('when locale is en-CA and timezone offset is 660', () => {
     const navigator = { language: 'en-CA' };
-    const date = { getTimezoneOffset: () => 660 };
     Object.defineProperty(global, 'navigator', { value: navigator });
-    Object.defineProperty(global, 'Date', { value: date });
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(660);
     mockParse.mockReturnValue({ region: 'CA' });
-    expect(localeHelper.isNorthAmerica).toBe(false);
+    expect(localeHelper.detectNorthAmerica()).toBe(false);
+    expect(mockConsoleError).not.toBeCalled();
   });
 
   test('when locale is zh-CN and timezone offset is 300', () => {
     const navigator = { language: 'zh-CN' };
-    const date = { getTimezoneOffset: () => 300 };
     Object.defineProperty(global, 'navigator', { value: navigator });
-    Object.defineProperty(global, 'Date', { value: date });
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(300);
     mockParse.mockReturnValue({ region: 'CN' });
-    expect(localeHelper.isNorthAmerica).toBe(false);
+    expect(localeHelper.detectNorthAmerica()).toBe(false);
+    expect(mockConsoleError).not.toBeCalled();
   });
 
   test('when locale is zh-CN and timezone offset is 660', () => {
     const navigator = { language: 'zh-CN' };
-    const date = { getTimezoneOffset: () => 660 };
     Object.defineProperty(global, 'navigator', { value: navigator });
-    Object.defineProperty(global, 'Date', { value: date });
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(660);
     mockParse.mockReturnValue({ region: 'CN' });
-    expect(localeHelper.isNorthAmerica).toBe(false);
+    expect(localeHelper.detectNorthAmerica()).toBe(false);
+    expect(mockConsoleError).not.toBeCalled();
   });
 });
--- src/web/helpers/locale-helper.ts
+++ src/web/helpers/locale-helper.ts
@@ -1,14 +1,19 @@
 import { parse } from 'bcp-47';
 
 const detectNorthAmerica = (): boolean => {
-  // @ts-expect-error incase some old browser use userLanguage
-  const userLocale = navigator.language || navigator.userLanguage;
-  const result = parse(userLocale);
-  if (result.region !== 'US' && result.region !== 'CA') return false;
-  const timezoneOffset = new Date().getTimezoneOffset();
-  // UTC-10 (Hawaii) to UTC-4 (Eastern Time Zone)
-  if (timezoneOffset > 600 || timezoneOffset < 240) return false;
-  return true;
+  try {
+    // @ts-expect-error incase some old browser use userLanguage
+    const userLocale = navigator.language || navigator.userLanguage;
+    const result = parse(userLocale);
+    if (result.region !== 'US' && result.region !== 'CA') return false;
+    const timezoneOffset = new Date().getTimezoneOffset();
+    // UTC-10 (Hawaii) to UTC-4 (Eastern Time Zone)
+    if (timezoneOffset > 600 || timezoneOffset < 240) return false;
+    return true;
+  } catch (e) {
+    console.error('Failed to get locale', e);
+    return true;
+  }
 };
 
 const isNorthAmerica = detectNorthAmerica();
