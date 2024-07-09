7e3ccb5d2c629c6a08d5529967e162c106605656
Dean Sung <dean@flux3dp.com>
2024-07-09 13:16:39 +0800

    Update rotary prespray position

--- src/web/helpers/api/svg-laser-parser.ts
+++ src/web/helpers/api/svg-laser-parser.ts
@@ -111,7 +111,7 @@ export default (parserOpts: { type?: string; onFatal?: (data) => void }) => {
         const { x, y, w, h } = presprayArea.getPosition(true);
         const workareaWidth = getWorkarea(model).width;
         args.push('-prespray');
-        args.push(rotaryMode ? `${workareaWidth - w},27,${w},${h}` : `${x},${y},${w},${h}`);
+        args.push(rotaryMode ? `${workareaWidth - 12},45,12,${h}` : `${x},${y},${w},${h}`);
         if (!isDevMode || BeamboxPreference.read('multipass-compensation') !== false)
           args.push('-mpc');
         if (!isDevMode || BeamboxPreference.read('one-way-printing') !== false) args.push('-owp');
