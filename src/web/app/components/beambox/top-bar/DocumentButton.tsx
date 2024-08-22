4a5ffc8be8a5c5e6f5c001510b3623597d636dd7
Dean Sung <dean@flux3dp.com>
2024-08-22 14:56:40 +0800

    Add DocumentButton test

--- src/web/app/components/beambox/top-bar/DocumentButton.tsx
+++ src/web/app/components/beambox/top-bar/DocumentButton.tsx
@@ -8,10 +8,10 @@ import styles from './DocumentButton.module.scss';
 
 
 const DocumentButton = (): JSX.Element => {
-  const lang = useI18n().topbar;
+  const lang = useI18n().topbar.menu;
 
   return (
-    <div className={styles.button} onClick={dialogCaller.showDocumentSettings} title={lang.task_preview}>
+    <div className={styles.button} onClick={dialogCaller.showDocumentSettings} title={lang.document_setting}>
       <TopBarIcons.Document />
     </div>
   );
