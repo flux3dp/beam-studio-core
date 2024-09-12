807c7c170427ff5770f416fcc7ce84085a922456
Dean Sung <dean@flux3dp.com>
2024-09-12 12:23:52 +0800

    Remove exported BeamboxPreference

--- src/web/app/actions/beambox/beambox-preference.ts
+++ src/web/app/actions/beambox/beambox-preference.ts
@@ -54,6 +54,4 @@ export const migrate = (): void => {
   if (instance.read('workarea') === 'fad1') instance.write('workarea', 'ado1');
 };
 
-window['BeamboxPreference'] = instance;
-
 export default instance;
