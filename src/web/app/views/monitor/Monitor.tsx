6ff27625aa68ffb90f13df313cb4efdb47c2ec30
Tuhacrt <a067800497@gmail.com>
2024-10-23 14:38:52 +0800

    feat(views-monitor): add toast message during file upload process.

--- src/web/app/views/monitor/Monitor.tsx
+++ src/web/app/views/monitor/Monitor.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react';
+import React from 'react';
 import { Modal, Tabs } from 'antd';
 import { CameraOutlined, FolderOutlined, PictureOutlined } from '@ant-design/icons';
 
@@ -8,11 +8,11 @@ import MonitorStatus from 'helpers/monitor-status';
 import useI18n from 'helpers/useI18n';
 import { Mode } from 'app/constants/monitor-constants';
 import { MonitorContext } from 'app/contexts/MonitorContext';
+import MessageCaller, { MessageLevel } from 'app/actions/message-caller';
 import { IDeviceInfo } from 'interfaces/IDevice';
 
 import MonitorCamera from './MonitorCamera';
 import MonitorFilelist from './MonitorFilelist';
-// import MonitorRelocate from './MonitorRelocate';
 import MonitorTabExtraContent from './MonitorTabExtraContent';
 import MonitorTask from './MonitorTask';
 
@@ -20,27 +20,30 @@ interface Props {
   device: IDeviceInfo;
 }
 
-const Monitor = (props: Props): JSX.Element => {
-  const { device } = props;
+const Monitor = ({ device }: Props): JSX.Element => {
   const { currentPath, mode, onClose, report, setMonitorMode, taskImageURL, uploadProgress } =
-    useContext(MonitorContext);
+    React.useContext(MonitorContext);
   const LANG = useI18n();
   const taskMode = report.st_id === deviceConstants.status.IDLE ? Mode.PREVIEW : Mode.WORKING;
   const monitorMode = [Mode.PREVIEW, Mode.FILE_PREVIEW, Mode.WORKING].includes(mode)
     ? taskMode
     : mode;
+  const [isUploadCompleted, setIsUploadCompleted] = React.useState(true);
+  const key = 'monitorUploadFileMessage';
 
   const tabItems = [
-    taskImageURL ? {
-      label: (
-        <div>
-          <PictureOutlined />
-          {LANG.monitor.taskTab}
-        </div>
-      ),
-      key: taskMode,
-      children: <MonitorTask />,
-    } : null,
+    taskImageURL
+      ? {
+          label: (
+            <div>
+              <PictureOutlined />
+              {LANG.monitor.taskTab}
+            </div>
+          ),
+          key: taskMode,
+          children: <MonitorTask />,
+        }
+      : null,
     {
       label: (
         <div>
@@ -51,21 +54,50 @@ const Monitor = (props: Props): JSX.Element => {
       key: Mode.FILE,
       children: <MonitorFilelist path={currentPath.join('/')} />,
     },
-    (localeHelper.isNorthAmerica ? null : {
-      label: (
-        <div>
-          <CameraOutlined />
-          {LANG.monitor.camera}
-        </div>
-      ),
-      key: Mode.CAMERA,
-      children: <MonitorCamera device={device} />,
-    }),
-  ].filter((item) => item) as { label: JSX.Element; key: Mode; children: JSX.Element }[]
+    localeHelper.isNorthAmerica
+      ? null
+      : {
+          label: (
+            <div>
+              <CameraOutlined />
+              {LANG.monitor.camera}
+            </div>
+          ),
+          key: Mode.CAMERA,
+          children: <MonitorCamera device={device} />,
+        },
+  ].filter(Boolean);
 
-  const statusText = useMemo(() => {
-    if (uploadProgress) return LANG.beambox.popup.progress.uploading;
-    if (report) return MonitorStatus.getDisplayStatus(report.st_label);
+  React.useEffect(() => {
+    if (uploadProgress) {
+      MessageCaller.openMessage({
+        key,
+        level: MessageLevel.LOADING,
+        content: `${LANG.beambox.popup.progress.uploading}...`,
+      });
+      setIsUploadCompleted(false);
+    } else if (report && !isUploadCompleted) {
+      MessageCaller.openMessage({
+        key,
+        level: MessageLevel.SUCCESS,
+        content: LANG.beambox.popup.successfully_uploaded,
+        duration: 2,
+        onClose: () => {
+          setIsUploadCompleted(true);
+        },
+      });
+    }
+  }, [LANG, isUploadCompleted, report, uploadProgress]);
+
+  const statusText = React.useMemo(() => {
+    if (uploadProgress) {
+      return LANG.beambox.popup.progress.uploading;
+    }
+
+    if (report) {
+      return MonitorStatus.getDisplayStatus(report.st_label);
+    }
+
     return LANG.monitor.connecting;
   }, [LANG, report, uploadProgress]);
 
@@ -74,7 +106,7 @@ const Monitor = (props: Props): JSX.Element => {
       <Tabs
         activeKey={monitorMode}
         items={tabItems}
-        onChange={(key: Mode) => setMonitorMode(key)}
+        onChange={setMonitorMode}
         tabBarExtraContent={<MonitorTabExtraContent />}
       />
     </Modal>
