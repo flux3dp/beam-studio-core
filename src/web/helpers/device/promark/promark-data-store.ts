2c346c428cccc64fdde86ab30f473a5abd4fd2dd
EstherFu <sloborfu@gmail.com>
2024-11-01 10:45:20 +0800

    Fix undefined storage value (empty string not trigger ?? operator)

--- src/web/helpers/device/promark/promark-data-store.ts
+++ src/web/helpers/device/promark/promark-data-store.ts
@@ -5,18 +5,18 @@ const get = <T extends keyof PromarkStore>(
   serial: string,
   key?: T
 ): T extends undefined ? PromarkStore : PromarkStore[T] => {
-  const store: PromarkStore = storage.get('promark-store')?.[serial] ?? {};
+  const store: PromarkStore = storage.get('promark-store')?.[serial] || {};
   return (key ? store[key] : store) as T extends undefined ? PromarkStore : PromarkStore[T];
 };
 
 const set = <T extends keyof PromarkStore>(serial: string, key: T, data: PromarkStore[T]): void => {
-  const store = storage.get('promark-store') ?? {};
+  const store = storage.get('promark-store') || {};
   store[serial] = { ...store[serial], [key]: data };
   storage.set('promark-store', store);
 };
 
 const update = (serial: string, data: Partial<PromarkStore>): void => {
-  const store = storage.get('promark-store') ?? {};
+  const store = storage.get('promark-store') || {};
   store[serial] = { ...store[serial], ...data };
   storage.set('promark-store', store);
 };
