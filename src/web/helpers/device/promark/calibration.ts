5e732a16b087f1d0c6d7a48af1387d767226e1e8
Dean Sung <dean@flux3dp.com>
2024-11-12 12:39:43 +0800

    Update generateCalibrationTaskString default params

--- src/web/helpers/device/promark/calibration.ts
+++ src/web/helpers/device/promark/calibration.ts
@@ -35,8 +35,8 @@ export const loadTaskToSwiftray = async (scene: string, model: WorkAreaModel): P
 export const generateCalibrationTaskString = async ({
   width,
   transform = '',
-  power = 20,
-  speed = 1000,
+  power = 100,
+  speed = 350,
 }: {
   width: number;
   transform?: string;
