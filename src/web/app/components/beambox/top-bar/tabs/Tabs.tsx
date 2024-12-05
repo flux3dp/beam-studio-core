0e1319b95247f57db708ec44c82ea5c04572dfd6
Dean Sung <dean@flux3dp.com>
2024-12-05 12:22:13 +0800

    Add droppableProvided.placeholder in Tabs

--- src/web/app/components/beambox/top-bar/tabs/Tabs.tsx
+++ src/web/app/components/beambox/top-bar/tabs/Tabs.tsx
@@ -96,6 +96,7 @@ const Tabs = (): JSX.Element => {
                   </Draggable>
                 );
               })}
+              {droppableProvided.placeholder}
             </div>
           )}
         </Droppable>
