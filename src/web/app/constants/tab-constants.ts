6d8b002727560a3df79819b6150f44b86a626239
Dean Sung <dean@flux3dp.com>
2024-12-09 17:02:00 +0800

    Rename tab contants

--- src/web/app/constants/tab-constants.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  // zero or undefined means no limit
-  maxTab: 6,
-};
---
+++ src/web/app/constants/tabConstants.ts
@@ -0,0 +1,9 @@
+export const enum TabEvents {
+  AddNewTab = 'add-new-tab',
+  FocusTab = 'focus-tab',
+}
+
+export default {
+  // zero or undefined means no limit
+  maxTab: 6,
+};
