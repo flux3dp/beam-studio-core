f010aef8b9644b871cfd2cb09f4b8e6f7a7d71de
Tuhacrt <a067800497@gmail.com>
2024-12-09 17:30:41 +0800

    test(material-test-generator): update test

--- src/web/app/components/dialogs/MaterialTestGeneratorPanel/BlockSetting.spec.ts
+++ src/web/app/components/dialogs/MaterialTestGeneratorPanel/BlockSetting.spec.ts
@@ -4,14 +4,14 @@ describe('MaterialTestGeneratorPanel BlockSetting', () => {
   it('should return initial value of block settings', () => {
     expect(blockSetting()).toEqual({
       column: {
-        count: { value: 10, min: 2, max: 20 },
-        size: { value: 10, min: 5, max: Number.MAX_SAFE_INTEGER },
-        spacing: { value: 5, min: 5, max: Number.MAX_SAFE_INTEGER },
+        count: { value: 10, min: 1, max: 20 },
+        size: { value: 10, min: 1, max: Number.MAX_SAFE_INTEGER },
+        spacing: { value: 5, min: 1, max: Number.MAX_SAFE_INTEGER },
       },
       row: {
-        count: { value: 10, min: 2, max: 20 },
-        size: { value: 10, min: 5, max: Number.MAX_SAFE_INTEGER },
-        spacing: { value: 5, min: 5, max: Number.MAX_SAFE_INTEGER },
+        count: { value: 10, min: 1, max: 20 },
+        size: { value: 10, min: 1, max: Number.MAX_SAFE_INTEGER },
+        spacing: { value: 5, min: 1, max: Number.MAX_SAFE_INTEGER },
       },
     });
   });
