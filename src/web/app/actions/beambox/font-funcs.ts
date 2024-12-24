b185691b8753d4aa966844bca946e428f5f87527
Dean Sung <dean@flux3dp.com>
2024-12-24 11:10:52 +0800

    Fix font-funcs for rtl texts

--- src/web/app/actions/beambox/font-funcs.ts
+++ src/web/app/actions/beambox/font-funcs.ts
@@ -235,21 +235,30 @@ export const convertTextToPathByFontkit = (
       /* eslint-enable no-param-reassign */
 
       const run = fontObj.layout(text);
+      const { glyphs, direction, positions } = run;
+      if (direction === 'rtl') {
+        glyphs.reverse();
+        positions.reverse();
+      }
       let i = 0;
-      d += run.glyphs
-        .map((char) => {
+      d += glyphs
+        .map((char, idx) => {
+          const pos = positions[idx];
           const start = textPath.getStartPositionOfChar(i);
-          const end = textPath.getStartPositionOfChar(i);
+          const end = textPath.getEndPositionOfChar(i);
           if ([start.x, start.y, end.x, end.y].every((v) => v === 0)) return '';
           const rot = (textPath.getRotationOfChar(i) / 180) * Math.PI;
+          const x = Math.min(start.x, end.x);
+          const y = Math.min(start.y, end.y);
           char.codePoints.forEach((codePoint) => {
             i = codePoint > maxChar ? i + 2 : i + 1;
           });
           return char.path
+            .translate(pos.xOffset, pos.yOffset)
             .scale(sizeRatio, -sizeRatio)
             .translate(0, alignOffset)
             .rotate(rot)
-            .translate(start.x, start.y)
+            .translate(x, y)
             .toSVG();
         })
         .join('');
@@ -259,14 +268,27 @@ export const convertTextToPathByFontkit = (
     tspans.forEach((tspan) => {
       const text = tspan.textContent;
       const run = fontObj.layout(text);
+      const { glyphs, direction, positions } = run;
+      if (direction === 'rtl') {
+        glyphs.reverse();
+        positions.reverse();
+      }
       let i = 0;
-      d += run.glyphs
-        .map((char) => {
+      d += glyphs
+        .map((char, idx) => {
+          const pos = positions[idx];
           const start = tspan.getStartPositionOfChar(i);
+          const end = tspan.getEndPositionOfChar(i);
+          const x = Math.min(start.x, end.x);
+          const y = Math.min(start.y, end.y);
           char.codePoints.forEach((codePoint) => {
             i = codePoint > maxChar ? i + 2 : i + 1;
           });
-          return char.path.scale(sizeRatio, -sizeRatio).translate(start.x, start.y).toSVG();
+          return char.path
+            .translate(pos.xOffset, pos.yOffset)
+            .scale(sizeRatio, -sizeRatio)
+            .translate(x, y)
+            .toSVG();
         })
         .join('');
     });
