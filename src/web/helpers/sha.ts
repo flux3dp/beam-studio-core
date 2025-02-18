93d8f3879edd0bbe724881271b7f39585c9ea536
EstherFu <sloborfu@gmail.com>
2025-02-18 14:54:28 +0800

    Fix restart and sha256

--- src/web/helpers/sha.ts
+++ src/web/helpers/sha.ts
@@ -1,9 +1,4 @@
-export const sha256 = async (message = ''): Promise<string> => {
-  const msgBuffer = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
-};
+import CryptoJS from 'crypto-js';
 
+export const sha256 = (message = ''): string => CryptoJS.SHA256(message).toString();
 export default { sha256 };
